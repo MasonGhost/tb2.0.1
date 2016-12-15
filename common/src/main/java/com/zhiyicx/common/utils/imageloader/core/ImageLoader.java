ecf99a438508eb0579f84a08f63e8cf0fd38e7ad
Jungle68 <335891510@qq.com>
2016-12-15 18:36:22 +0800

    使用daager2的单例模式管理imageloader

--- common/src/main/java/com/zhiyicx/common/utils/imageloader/core/ImageLoader.java
+++ common/src/main/java/com/zhiyicx/common/utils/imageloader/core/ImageLoader.java
@@ -13,22 +13,11 @@ import com.zhiyicx.common.utils.imageloader.loadstrategy.ImageLoaderStrategy;
  */
 
 public class ImageLoader {
-    //    private volatile static ImageLoader mImageLoader;
     private ImageLoaderStrategy mStrategy;
 
     public ImageLoader(ImageLoaderStrategy strategy) {
         setLoadImgStrategy(strategy);
     }
-
-//    public static ImageLoader getInstance() {
-//        if (mImageLoader == null) {
-//            synchronized (ImageLoader.class) {
-//                if (mImageLoader == null)
-//                    mImageLoader = new ImageLoader();
-//            }
-//        }
-//        return mImageLoader;
-//    }
 
     public <T extends ImageConfig> void loadImage(Context context, T config) {
         this.mStrategy.loadImage(context, config);
