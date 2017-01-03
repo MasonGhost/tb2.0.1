8f5e3e0c1acde480e88cc536c420164ed29d214b
Jungle68 <335891510@qq.com>
2017-01-03 10:10:33 +0800

    (#28) 测试 手机号是否可以为空

--- app/src/androidTest/java/com/zhiyicx/thinksnsplus/modules/register/RegisterActivityTest.java
+++ app/src/androidTest/java/com/zhiyicx/thinksnsplus/modules/register/RegisterActivityTest.java
@@ -52,8 +52,8 @@ public class RegisterActivityTest {
     /**
      * summary                       用户名字符长度是否有限制(最长、最短)
      * steps                         1. 用户名输入文字若干
-     *                               2. 填写正确手机号、验证码、密码
-     *                               3. 点击注册
+     * 2. 填写正确手机号、验证码、密码
+     * 3. 点击注册
      * expected                      字符为空时，不能点击“注册”
      * 用户名达到8位后，不能再输入
      *
@@ -74,10 +74,10 @@ public class RegisterActivityTest {
     /**
      * summary                       用户名字符长度是否有限制(最长、最短)
      * steps                         1. 用户名输入文字若干
-     *                               2. 填写正确手机号、验证码、密码
-     *                               3. 点击注册
+     * 2. 填写正确手机号、验证码、密码
+     * 3. 点击注册
      * expected                      用户名过短，提示应为 2-8 个字符；
-     *                                  用户名达到8位后，不能再输入
+     * 用户名达到8位后，不能再输入
      *
      * @throws Exception
      */
@@ -96,10 +96,9 @@ public class RegisterActivityTest {
     /**
      * summary                       用户名字符长度是否有限制(最长、最短)
      * steps                         1. 用户名输入文字若干
-     *                               2. 填写正确手机号、验证码、密码
-     *                               3. 点击注册
+     * 2. 填写正确手机号、验证码、密码
+     * 3. 点击注册
      * expected                      用户名可以包含数字
-     *
      *
      * @throws Exception
      */
@@ -117,10 +116,9 @@ public class RegisterActivityTest {
     /**
      * summary                       用户名字符长度是否有限制(最长、最短)
      * steps                         1. 用户名输入文字若干
-     *                               2. 填写正确手机号、验证码、密码
-     *                               3. 点击注册
+     * 2. 填写正确手机号、验证码、密码
+     * 3. 点击注册
      * expected                      2-8 个字符正确注册
-     *
      *
      * @throws Exception
      */
@@ -134,14 +132,19 @@ public class RegisterActivityTest {
         onView(withId(R.id.bt_regist_regist)).check(matches(isEnabled())).perform(click());
         onView(withId(R.id.tv_error_tip)).check(matches(isDisappear()));
     }
+
+    /************************************** 手机号 ************************************/
+
+
     /**
      * summary                       是否可输入不合法的手机号码
-     *
+     * <p>
      * steps                             ① 输入用户名“啊哈”
-                                         ② 手机号：12345685
-                                         ③ 发送验证码;
-
+     * ② 手机号：12345685
+     * ③ 发送验证码;
+     * <p>
      * expected                       发送验证码按钮不亮
+     *
      * @throws Exception
      */
     @Test
@@ -150,14 +153,16 @@ public class RegisterActivityTest {
         onView(withId(R.id.et_regist_username)).perform(replaceText(TEST_USER_NAME));
         onView(withId(R.id.bt_regist_send_vertify_code)).check(matches(disEnabled()));
     }
+
     /**
      * summary                       是否可输入不合法的手机号码
-     *
+     * <p>
      * steps                             ① 输入用户名“啊哈”
-                                         ② 手机号：
-                                         ③ 发送验证码
-
+     * ② 手机号：
+     * ③ 发送验证码
+     * <p>
      * expected                     提示请输入正确的手机号
+     *
      * @throws Exception
      */
     @Test
@@ -168,4 +173,19 @@ public class RegisterActivityTest {
         onView(withId(R.id.tv_error_tip)).check(matches(withText(mActivityRule.getActivity().getString(R.string.phone_number_toast_hint))));
     }
 
+    /**
+     * summary                      手机号是否可以为空
+     * <p>
+     * steps                             手机号不填
+     * <p>
+     * expected                     发验证码按钮不亮，无法点击
+     *
+     * @throws Exception
+     */
+    @Test
+    public void errorPhoneNumber_null() throws Exception {
+        onView(withId(R.id.et_regist_username)).perform(replaceText(TEST_USER_NAME));
+        onView(withId(R.id.et_regist_phone)).perform(typeText(""));
+        onView(withId(R.id.bt_regist_send_vertify_code)).check(matches(disEnabled()));
+    }
 }
