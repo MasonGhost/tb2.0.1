f55d5aaba52a86116b62edf73823d32e2fd0eab0
Jungle68 <335891510@qq.com>
2017-01-05 18:21:42 +0800

    （#18） code 聊天列表视图与示例

--- app/src/main/java/com/zhiyicx/thinksnsplus/modules/home/MessageFragment.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/home/MessageFragment.java
@@ -3,11 +3,17 @@ package com.zhiyicx.thinksnsplus.modules.home;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.zhiyicx.baseproject.base.TSFragment;
+import com.zhiyicx.common.utils.ConvertUtils;
+import com.zhiyicx.common.utils.recycleviewdecoration.LinearDecoration;
 import com.zhiyicx.thinksnsplus.R;
 import com.zhy.adapter.recyclerview.CommonAdapter;
 import com.zhy.adapter.recyclerview.base.ViewHolder;
@@ -48,17 +54,30 @@ public class MessageFragment extends TSFragment {
     @Override
     protected void initView(View rootView) {
         mToolbarCenter.setTextColor(ContextCompat.getColor(getContext(), R.color.important_for_content));
-        mHeaderView= LayoutInflater.from(getActivity()).inflate(R.layout.header_message_list,null);
+        mHeaderView = LayoutInflater.from(getActivity()).inflate(R.layout.header_message_list, null);
     }
 
 
     @Override
     protected void initData() {
-        List<String> mDatas=new ArrayList<>();
+        List<String> mDatas = new ArrayList<>();
+        mDatas.add("nihao"); // 测试数据，暂时使用
+        mDatas.add("nihao1");
+        mDatas.add("nihao2");
+        mDatas.add("nihao3");
+        mRvMessageList.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRvMessageList.addItemDecoration(new LinearDecoration(0, ConvertUtils.dp2px(getContext(), 10f), 0, 0));//设置Item的间隔
+        //如果可以确定每个item的高度是固定的，设置这个选项可以提高性能
+        mRvMessageList.setHasFixedSize(true);
+        mRvMessageList.setItemAnimator(new DefaultItemAnimator());//设置动画
         mRvMessageList.setAdapter(new CommonAdapter<String>(getActivity(), R.layout.item_message_list, mDatas) {
             @Override
             protected void convert(ViewHolder holder, String s, int position) {
-
+                Glide.with(getContext()).load(R.mipmap.ico_eye_open).into((ImageView) holder.getView(R.id.iv_headpic));
+                holder.setText(R.id.tv_name,"张三");
+                holder.setText(R.id.tv_content,"我的天的道德观念我的是高科技的思考国际快递发几个客服房间打开数据库");
+                holder.setText(R.id.tv_time,"2016-05-06");
+                holder.setText(R.id.tv_tip,"109");
             }
         });
     }
