3ddfa49400e121cd6939599ac1d40e95182c8b46
Jungle68 <335891510@qq.com>
2017-01-05 15:06:08 +0800

    （#18） code  增加聊天列表布局

--- app/src/main/java/com/zhiyicx/thinksnsplus/modules/home/MessageFragment.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/home/MessageFragment.java
@@ -3,10 +3,16 @@ package com.zhiyicx.thinksnsplus.modules.home;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.zhiyicx.baseproject.base.TSFragment;
 import com.zhiyicx.thinksnsplus.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * @Describe 消息页面
@@ -15,6 +21,9 @@ import com.zhiyicx.thinksnsplus.R;
  * @Contact master.jungle68@gmail.com
  */
 public class MessageFragment extends TSFragment {
+    @BindView(R.id.rv_message_list)
+    RecyclerView mRvMessageList;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +42,7 @@ public class MessageFragment extends TSFragment {
 
     @Override
     protected void initView(View rootView) {
-        mToolbarCenter.setTextColor(ContextCompat.getColor(getContext(),R.color.important_for_content));
+        mToolbarCenter.setTextColor(ContextCompat.getColor(getContext(), R.color.important_for_content));
     }
 
 
@@ -61,4 +70,5 @@ public class MessageFragment extends TSFragment {
     protected boolean showToolBarDivider() {
         return true;
     }
+
 }
