77ee7e9be240a0d7b4d9cb5c825308e7c34b01d2
Jungle68 <335891510@qq.com>
2017-01-09 17:24:46 +0800

    (# 44) code 增加设置页Item响应事件

--- app/src/main/java/com/zhiyicx/thinksnsplus/modules/settings/SettingsFragment.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/settings/SettingsFragment.java
@@ -2,9 +2,19 @@ package com.zhiyicx.thinksnsplus.modules.settings;
 
 import android.view.View;
 
+import com.jakewharton.rxbinding.view.RxView;
 import com.zhiyicx.baseproject.base.TSFragment;
+import com.zhiyicx.baseproject.widget.CombinationButton;
+import com.zhiyicx.common.utils.ToastUtils;
 import com.zhiyicx.thinksnsplus.R;
 import com.zhiyicx.thinksnsplus.modules.register.RegisterContract;
+
+import java.util.concurrent.TimeUnit;
+
+import butterknife.BindView;
+import rx.functions.Action1;
+
+import static com.zhiyicx.common.config.ConstantConfig.JITTER_SPACING_TIME;
 
 /**
  * @Describe
@@ -13,6 +23,17 @@ import com.zhiyicx.thinksnsplus.modules.register.RegisterContract;
  * @Contact master.jungle68@gmail.com
  */
 public class SettingsFragment extends TSFragment<RegisterContract.Presenter> implements RegisterContract.View {
+
+    @BindView(R.id.bt_login_out)
+    CombinationButton mBtLoginOut;
+    @BindView(R.id.bt_set_vertify)
+    CombinationButton mBtSetVertify;
+    @BindView(R.id.bt_change_password)
+    CombinationButton mBtChangePassword;
+    @BindView(R.id.bt_clean_cache)
+    CombinationButton mBtCleanCache;
+    @BindView(R.id.bt_about_us)
+    CombinationButton mBtAboutUs;
 
     public static SettingsFragment newInstance() {
         SettingsFragment fragment = new SettingsFragment();
@@ -41,6 +62,7 @@ public class SettingsFragment extends TSFragment<RegisterContract.Presenter> imp
 
     @Override
     protected void initView(View rootView) {
+        initListener();
     }
 
     @Override
@@ -82,7 +104,7 @@ public class SettingsFragment extends TSFragment<RegisterContract.Presenter> imp
 
     @Override
     public void showMessage(String message) {
-
+        ToastUtils.showToast(message);
     }
 
 
@@ -93,4 +115,56 @@ public class SettingsFragment extends TSFragment<RegisterContract.Presenter> imp
 
     }
 
+    private void initListener() {
+        // 认证
+        RxView.clicks(mBtSetVertify)
+                .throttleFirst(JITTER_SPACING_TIME, TimeUnit.SECONDS)   //两秒钟之内只取一个点击事件，防抖操作
+                .compose(this.<Void>bindToLifecycle())
+                .subscribe(new Action1<Void>() {
+                    @Override
+                    public void call(Void aVoid) {
+                        showMessage("vertify");
+                    }
+                });
+        // 修改密码
+        RxView.clicks(mBtChangePassword)
+                .throttleFirst(JITTER_SPACING_TIME, TimeUnit.SECONDS)   //两秒钟之内只取一个点击事件，防抖操作
+                .compose(this.<Void>bindToLifecycle())
+                .subscribe(new Action1<Void>() {
+                    @Override
+                    public void call(Void aVoid) {
+                        showMessage("change_password");
+                    }
+                });
+        // 清理缓存
+        RxView.clicks(mBtCleanCache)
+                .throttleFirst(JITTER_SPACING_TIME, TimeUnit.SECONDS)   //两秒钟之内只取一个点击事件，防抖操作
+                .compose(this.<Void>bindToLifecycle())
+                .subscribe(new Action1<Void>() {
+                    @Override
+                    public void call(Void aVoid) {
+                        showMessage("clean_cache");
+                    }
+                });
+        // 关于我们
+        RxView.clicks(mBtAboutUs)
+                .throttleFirst(JITTER_SPACING_TIME, TimeUnit.SECONDS)   //两秒钟之内只取一个点击事件，防抖操作
+                .compose(this.<Void>bindToLifecycle())
+                .subscribe(new Action1<Void>() {
+                    @Override
+                    public void call(Void aVoid) {
+                        showMessage("about_us");
+                    }
+                });
+        // 退出登录
+        RxView.clicks(mBtLoginOut)
+                .throttleFirst(JITTER_SPACING_TIME, TimeUnit.SECONDS)   //两秒钟之内只取一个点击事件，防抖操作
+                .compose(this.<Void>bindToLifecycle())
+                .subscribe(new Action1<Void>() {
+                    @Override
+                    public void call(Void aVoid) {
+                        showMessage("login_out");
+                    }
+                });
+    }
 }
