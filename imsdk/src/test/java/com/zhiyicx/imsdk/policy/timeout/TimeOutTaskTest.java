b5c738658f867d96c42c8c1b2a605e1153d7902c
Jungle68 <335891510@qq.com>
2017-01-16 15:55:16 +0800

    (#14) test 增加消息自动重发测试

--- imsdk/src/test/java/com/zhiyicx/imsdk/policy/timeout/TimeOutTaskTest.java
+++ imsdk/src/test/java/com/zhiyicx/imsdk/policy/timeout/TimeOutTaskTest.java
@@ -15,11 +15,12 @@ import org.junit.Test;
  */
 public class TimeOutTaskTest {
     /**
-     *  超时任务测试
+     * 超时任务测试
+     *
      * @throws Exception
      */
     @Test
-    public void testTimeOut() throws Exception{
+    public void testTimeOut() throws Exception {
         //并启动TimeOut线程池
         TimeOutTaskPool timeOutTaskPool = new TimeOutTaskPool();
         new Thread(timeOutTaskPool).start();
@@ -27,18 +28,20 @@ public class TimeOutTaskTest {
         TimeOutTask timeOutTask = new TimeOutTask(new MessageContainer(ImService.CONVR_MSG_PLUCK, new Message(123), 100, null), System.currentTimeMillis(), new TimeOutListener() {
             @Override
             public void timeOut(MessageContainer messageContainer) {
-                Assert.assertTrue(messageContainer.msg.id==123);
+                Assert.assertTrue(messageContainer.msg.id == 123);
             }
         });
         TimeOutTaskManager.getInstance().addTimeoutTask(timeOutTask);
-        Thread.sleep(TimeOutTask.OUT_TIME+10);
+        Thread.sleep(TimeOutTask.OUT_TIME + 10);
     }
+
     /**
-     *  取消超时任务测试
+     * 取消超时任务测试
+     *
      * @throws Exception
      */
     @Test
-    public void testCancleTimeOut() throws Exception{
+    public void testCancleTimeOut() throws Exception {
         //并启动TimeOut线程池
         TimeOutTaskPool timeOutTaskPool = new TimeOutTaskPool();
         new Thread(timeOutTaskPool).start();
@@ -50,7 +53,28 @@ public class TimeOutTaskTest {
             }
         });
         TimeOutTaskManager.getInstance().addTimeoutTask(timeOutTask);
-        TimeOutTaskManager.getInstance().cancleTimeoutTask(123+"");
-        Thread.sleep(TimeOutTask.OUT_TIME+10);
+        TimeOutTaskManager.getInstance().cancleTimeoutTask(123 + "");
+        Thread.sleep(TimeOutTask.OUT_TIME + 10);
+    }
+
+    /**
+     * 消息自动重复次数测试
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testReSend() throws Exception {
+        //并启动TimeOut线程池
+        TimeOutTaskPool timeOutTaskPool = new TimeOutTaskPool();
+        new Thread(timeOutTaskPool).start();
+
+        TimeOutTask timeOutTask = new TimeOutTask(new MessageContainer(ImService.CONVR_MSG_PLUCK, new Message(123), 100, null), System.currentTimeMillis(), new TimeOutListener() {
+            @Override
+            public void timeOut(MessageContainer messageContainer) {
+                Assert.assertTrue(messageContainer.reSendCounts == 1);
+            }
+        });
+        TimeOutTaskManager.getInstance().addTimeoutTask(timeOutTask);
+        Thread.sleep(TimeOutTask.OUT_TIME + 10);
     }
 }
