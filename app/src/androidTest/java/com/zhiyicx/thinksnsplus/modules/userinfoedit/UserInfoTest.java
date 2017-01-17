91db5a9c0b43047f9d27f27d2b1b1702fd10180a
LC\ZYCX <450127106@qq.com>
2017-01-17 17:20:02 +0800

    （#20）test 添加用户信息编辑的测试类

---
+++ app/src/androidTest/java/com/zhiyicx/thinksnsplus/modules/userinfoedit/UserInfoTest.java
@@ -0,0 +1,25 @@
+package com.zhiyicx.thinksnsplus.modules.userinfoedit;
+
+import com.zhiyicx.thinksnsplus.modules.AcitivityTest;
+
+import org.junit.Test;
+
+/**
+ * @author LiuChao
+ * @describe  用户信息编辑测试
+ * @date 2017/1/17
+ * @contact email:450127106@qq.com
+ */
+
+public class UserInfoTest extends AcitivityTest{
+    /**
+     * summary
+     * steps
+     * expected
+     */
+    @Test
+    public void name() throws Exception {
+
+
+    }
+}
