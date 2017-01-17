dffc003b85aed3c2dced298feb6f6887b5048441
LC\ZYCX <450127106@qq.com>
2017-01-17 13:36:36 +0800

    （#20） code 添加用户信息编辑的简介输入自定义布局

---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/widget/UserInfoInroduceInputView.java
@@ -0,0 +1,11 @@
+package com.zhiyicx.thinksnsplus.widget;
+
+/**
+ * @author LiuChao
+ * @describe
+ * @date 2017/1/17
+ * @contact email:450127106@qq.com
+ */
+
+public class UserInfoInroduceInputView {
+}
