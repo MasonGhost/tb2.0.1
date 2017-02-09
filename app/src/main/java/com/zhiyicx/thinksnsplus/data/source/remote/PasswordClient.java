431a8aadd8563e8996a19489a46fbecf603ce16c
LC\ZYCX <450127106@qq.com>
2017-02-09 15:27:23 +0800

    （#50 #49 ）code 接口调整

--- app/src/main/java/com/zhiyicx/thinksnsplus/data/source/remote/PasswordClient.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/data/source/remote/PasswordClient.java
@@ -5,6 +5,7 @@ import com.zhiyicx.common.base.BaseJson;
 
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
 import rx.Observable;
@@ -29,7 +30,7 @@ public interface PasswordClient {
      * @return
      */
     @FormUrlEncoded
-    @POST(APP_PATH_CHANGE_PASSWORD)
+    @PATCH(APP_PATH_CHANGE_PASSWORD)
     Observable<BaseJson<CacheBean>> changePassword(@Query("requestState") String requestState, @Field("password") String password
             , @Field("new_password") String newPassword);
 
@@ -41,7 +42,7 @@ public interface PasswordClient {
      * @return
      */
     @FormUrlEncoded
-    @POST(APP_PATH_FIND_PASSWORD)
+    @PATCH(APP_PATH_FIND_PASSWORD)
     Observable<BaseJson<CacheBean>> findPassword(@Query("requestState") String requestState, @Field("phone") String phone
           , @Field("code") String vertifyCode, @Field("password") String newPassword);
 }
