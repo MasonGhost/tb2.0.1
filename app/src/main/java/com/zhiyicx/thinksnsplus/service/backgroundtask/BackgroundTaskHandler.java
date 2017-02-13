88b1a75730a78602ff07b6b2e3027080df5b625f
Jungle68 <335891510@qq.com>
2017-02-13 17:34:34 +0800

    (#18) code 修改本地聊天地址

--- app/src/main/java/com/zhiyicx/thinksnsplus/service/backgroundtask/BackgroundTaskHandler.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/service/backgroundtask/BackgroundTaskHandler.java
@@ -180,7 +180,7 @@ public class BackgroundTaskHandler {
                                 IMConfig imConfig = new IMConfig();
                                 imConfig.setImUid(data.getUser_id());
                                 imConfig.setToken(data.getIm_password());
-                                imConfig.setWeb_socket_authority("ws://192.168.10.222:9900"); // TODO: 2017/1/20  服务器统一配置接口返回数据
+                                imConfig.setWeb_socket_authority("ws://192.168.2.222:9900"); // TODO: 2017/1/20  服务器统一配置接口返回数据 ws://218.244.149.144:9900
                                 ZBIMClient.getInstance().login(imConfig);
                             }
 
