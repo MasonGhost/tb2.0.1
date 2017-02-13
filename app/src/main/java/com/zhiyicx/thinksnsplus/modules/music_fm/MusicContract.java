a9eb762d10f8b3e4f0272f29fc948859cf3523db
Jliuer <jliuer@aliyun.com>
2017-02-13 18:07:03 +0800

    (#73) code 创建音乐FM首页的mvp相关类

---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/music_fm/MusicContract.java
@@ -0,0 +1,33 @@
+package com.zhiyicx.thinksnsplus.modules.music_fm;
+
+import com.zhiyicx.baseproject.base.ITSListPresenter;
+import com.zhiyicx.common.base.BaseJson;
+import com.zhiyicx.common.mvp.i.IBasePresenter;
+import com.zhiyicx.common.mvp.i.IBaseView;
+import com.zhiyicx.thinksnsplus.data.beans.MusicListBean;
+
+import java.util.Map;
+
+import rx.Observable;
+
+/**
+ * @Author Jliuer
+ * @Date 2017/02/13
+ * @Email Jliuer@aliyun.com
+ * @Description 音乐FM 契约类
+ */
+
+public interface MusicContract {
+
+    interface View extends IBaseView<Presenter> {
+
+    }
+
+    interface Presenter extends ITSListPresenter {
+        void getMusicList();
+    }
+
+    interface Repository {
+        Observable<BaseJson<MusicListBean>> getMusicList(Map map);
+    }
+}
