fdf9c81049941b8ec0bd9b7ed7a27058ce8c02e6
Jliuer <jliuer@aliyun.com>
2017-02-13 18:09:53 +0800

    (#73) code 创建音乐FM首页dagger相关类

---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/music_fm/MusicPresenterModule.java
@@ -0,0 +1,32 @@
+package com.zhiyicx.thinksnsplus.modules.music_fm;
+
+import com.zhiyicx.thinksnsplus.data.source.remote.ServiceManager;
+import com.zhiyicx.thinksnsplus.data.source.repository.MusicRepository;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * @Author Jliuer
+ * @Date 2017/02/13
+ * @Email Jliuer@aliyun.com
+ * @Description
+ */
+@Module
+class MusicPresenterModule {
+    private MusicContract.View view;
+
+    public MusicPresenterModule(MusicContract.View view) {
+        this.view = view;
+    }
+
+    @Provides
+    MusicContract.View provideMusicContractView() {
+        return view;
+    }
+
+    @Provides
+    MusicContract.Repository provideMusicRepository(ServiceManager serviceManager) {
+        return new MusicRepository(serviceManager);
+    }
+}
