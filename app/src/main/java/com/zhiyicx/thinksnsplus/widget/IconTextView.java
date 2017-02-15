35300bac3cf185f5431d0c8a1432626f65170e84
Jliuer <jliuer@aliyun.com>
2017-02-15 18:30:24 +0800

    (#73) code 完成带有图标的textview组建,嵌套滑动组建

--- app/src/main/java/com/zhiyicx/thinksnsplus/widget/IconTextView.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/widget/IconTextView.java
@@ -18,7 +18,7 @@ import com.zhiyicx.thinksnsplus.R;
  * @Author Jliuer
  * @Date 2017/02/14
  * @Email Jliuer@aliyun.com
- * @Description 有待扩展 icon 位置方向，目前仅支持左边
+ * @Description 有待扩展 icon 大小修改
  */
 public class IconTextView extends TextView {
 
@@ -40,7 +40,16 @@ public class IconTextView extends TextView {
      * 处理后目标区域
      */
     private Rect target;
-    private Rect bound ;
+
+    private static final int DIRECTION_LEFT = 1;
+    private static final int DIRECTION_TOP = 2;
+    private static final int DIRECTION_RIGHT = 3;
+    private static final int DIRECTION_BOTTOM = 4;
+
+    /**
+     * 方向
+     */
+    private int direction = -1;
 
     public IconTextView(Context context) {
         super(context);
@@ -55,14 +64,17 @@ public class IconTextView extends TextView {
     private void init(Context context, AttributeSet attrs) {
         if (attrs != null) {
             TypedArray array = context.obtainStyledAttributes(attrs,
-                    com.zhiyicx.baseproject.R.styleable.topTipView);
+                    com.zhiyicx.baseproject.R.styleable.IconTextView);
             mIconID = array.getResourceId(com.zhiyicx.baseproject.R.styleable
-                    .IconTextView_srcIcon, R.mipmap.npc);
+                    .IconTextView_srcIcon, 0);
             if (mIconID > 0) {
                 mBitmap = BitmapFactory.decodeResource(getResources(), mIconID);
+                mSpace = array.getDimension(com.zhiyicx.baseproject.R.styleable.IconTextView_space,
+                        0);
+                direction = array.getInteger(com.zhiyicx.baseproject.R.styleable
+                        .IconTextView_direction,
+                        DIRECTION_LEFT);
             }
-//            mSpace = array.getDimension(com.zhiyicx.baseproject.R.styleable.IconTextView_space,
-//                    0);
             array.recycle();
         }
     }
@@ -75,25 +87,47 @@ public class IconTextView extends TextView {
             if (src == null) {
                 target = new Rect();
                 src = new Rect();
-                bound = new Rect();
             }
             src.left = 0;
             src.top = 0;
             src.right = width;
             src.bottom = height;
             int textHeight = (int) getTextSize();
-            target.left =0;
+
+            target.left = 0;
 
             target.top = (int) ((getMeasuredHeight() - getTextSize()) / 2) + 1;
             target.bottom = target.top + textHeight;
 
             //保证图像不变形
             target.right = (int) (textHeight * (width / (float) height));
+
+            float textLeft = getLayout().getPrimaryHorizontal(0);
+            float textWidth = getPaint().measureText(getText().toString());
+            float textRight = textWidth + textLeft;
+
+
+            int iconWidth = target.right - target.left;
+            int iconHeight = target.bottom - target.top;
+
             canvas.save();
-            canvas.translate(getLayout().getPrimaryHorizontal(0)-target.right,0);
+            if (direction == DIRECTION_LEFT) {
+                canvas.translate(textLeft - target.right - mSpace, 0);
+            } else if (direction == DIRECTION_TOP) {
+                canvas.translate(textLeft + (textWidth / 2 - iconWidth / 2), target.top - target
+                        .bottom
+                        - mSpace);
+            } else if (direction == DIRECTION_RIGHT) {
+                canvas.translate(mSpace + textRight, 0);
+            } else if (direction == DIRECTION_BOTTOM) {
+                canvas.translate(textLeft + (textWidth / 2 - iconWidth / 2), mSpace + target
+                        .bottom -
+                        target.top);
+            }
             canvas.drawBitmap(mBitmap, src, target, getPaint());
             canvas.restore();
         }
         super.onDraw(canvas);
     }
+
 }
