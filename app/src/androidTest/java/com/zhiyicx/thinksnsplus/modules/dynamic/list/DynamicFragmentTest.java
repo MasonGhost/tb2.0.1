f3a2eb3865622a84b7619d4184d2162fcd564eaa
Jungle68 <335891510@qq.com>
2017-02-23 18:42:28 +0800

    (#86) test 添加动态列表UI测试

--- app/src/androidTest/java/com/zhiyicx/thinksnsplus/modules/dynamic/list/DynamicFragmentTest.java
+++ app/src/androidTest/java/com/zhiyicx/thinksnsplus/modules/dynamic/list/DynamicFragmentTest.java
@@ -4,12 +4,18 @@ import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.zhiyicx.thinksnsplus.R;
 import com.zhiyicx.thinksnsplus.modules.home.HomeActivity;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 /**
  * @Describe
@@ -39,8 +45,19 @@ public class DynamicFragmentTest {
      */
     @Test
     public void testDynamicActivity() throws Exception {
-        DynamicFragment dynamicFragment=DynamicFragment.newInstance();
-        dynamicFragment.showMessage("this is a test message");
+        onView(withId(R.id.tsv_toolbar)).check(matches(isDisplayed()));
+    }
+
+    /**
+     * summary              当主页显示的时候
+     * steps                查看首页类容
+     * expected              首页分类正常显示
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testHomeNoBack() throws Exception {
+        onView(withId(R.id.mg_indicator)).check(matches(isDisplayed()));
     }
 
 }
