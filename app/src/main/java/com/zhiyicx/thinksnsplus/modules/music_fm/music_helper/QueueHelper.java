86e201abf3483e3380a08304cdf39c949ab3e5d8
Jliuer <jliuer@aliyun.com>
2017-02-24 19:12:20 +0800

    (#87) code 歌曲播放模式切换

--- app/src/main/java/com/zhiyicx/thinksnsplus/modules/music_fm/music_helper/QueueHelper.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/music_fm/music_helper/QueueHelper.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2014 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.zhiyicx.thinksnsplus.modules.music_fm.music_helper;
 
 import android.content.Context;
@@ -23,7 +7,6 @@ import android.support.v4.media.MediaMetadataCompat;
 import android.support.v4.media.session.MediaControllerCompat;
 import android.support.v4.media.session.MediaSessionCompat;
 import android.text.TextUtils;
-
 
 import com.zhiyicx.thinksnsplus.modules.music_fm.media_data.MusicProvider;
 import com.zhiyicx.thinksnsplus.modules.music_fm.media_data.MusicSearchParams;
@@ -43,8 +26,6 @@ import static com.zhiyicx.thinksnsplus.modules.music_fm.music_helper.MediaIDHelp
  * @Description 播放队列
  */
 public class QueueHelper {
-
-    private static final int RANDOM_QUEUE_SIZE = 10;
 
     public static List<MediaSessionCompat.QueueItem> getPlayingQueue(String mediaId,
                                                                      MusicProvider musicProvider) {
@@ -142,15 +123,11 @@ public class QueueHelper {
     }
 
     public static List<MediaSessionCompat.QueueItem> getRandomQueue(MusicProvider musicProvider) {
-        List<MediaMetadataCompat> result = new ArrayList<>(RANDOM_QUEUE_SIZE);
         Iterable<MediaMetadataCompat> shuffled = musicProvider.getShuffledMusic();
+        List<MediaMetadataCompat> result = new ArrayList<>();
         for (MediaMetadataCompat metadata : shuffled) {
-            if (result.size() == RANDOM_QUEUE_SIZE) {
-                break;
-            }
             result.add(metadata);
         }
-
         return convertToQueue(result, MEDIA_ID_MUSICS_BY_SEARCH, "random");
     }
 
