664713ca23f7694a86b3088ca71ecadc9075a6a1
LC\ZYCX <450127106@qq.com>
2017-03-01 10:56:42 +0800

    （#104）code 修改动态详情基类

--- app/src/main/java/com/zhiyicx/thinksnsplus/modules/dynamic/detail/DynamicDetailFragment.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/dynamic/detail/DynamicDetailFragment.java
@@ -4,7 +4,15 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.zhiyicx.baseproject.base.TSFragment;
+import com.zhiyicx.baseproject.base.TSListFragment;
 import com.zhiyicx.thinksnsplus.R;
+import com.zhiyicx.thinksnsplus.data.beans.DynamicBean;
+import com.zhiyicx.thinksnsplus.modules.dynamic.detail.adapter.DynamicDetailItemForContent;
+import com.zhiyicx.thinksnsplus.modules.dynamic.detail.adapter.DynamicDetailItemForDig;
+import com.zhy.adapter.recyclerview.MultiItemTypeAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author LiuChao
@@ -13,7 +21,10 @@ import com.zhiyicx.thinksnsplus.R;
  * @contact email:450127106@qq.com
  */
 
-public class DynamicDetailFragment extends TSFragment<DynamicDetailContract.Presenter> implements DynamicDetailContract.View {
+public class DynamicDetailFragment extends TSListFragment<DynamicDetailContract.Presenter, DynamicBean> implements DynamicDetailContract.View {
+
+    private List<DynamicBean> mDatas = new ArrayList<>();
+
     @Override
     protected int getBodyLayoutId() {
         return R.layout.fragment_dynamic_detail;
@@ -27,6 +38,14 @@ public class DynamicDetailFragment extends TSFragment<DynamicDetailContract.Pres
     @Override
     protected void initData() {
 
+    }
+
+    @Override
+    protected MultiItemTypeAdapter<DynamicBean> getAdapter() {
+        MultiItemTypeAdapter<DynamicBean> adapter = new MultiItemTypeAdapter<>(getContext(), mDatas);
+        adapter.addItemViewDelegate(new DynamicDetailItemForContent());
+        adapter.addItemViewDelegate(new DynamicDetailItemForDig());
+        return adapter;
     }
 
     @Override
