7a243500e91f9d2322061ccc851f90e3fd9cdb06
LC\ZYCX <450127106@qq.com>
2017-03-03 11:40:25 +0800

    (#104) code 添加点赞榜页面，暂时放一放

---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/dynamic/detail/dig_list/DigListActivity.java
@@ -0,0 +1,14 @@
+package com.zhiyicx.thinksnsplus.modules.dynamic.detail.dig_list;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zhiyicx.baseproject.base.TSActivity;
+
+public class DigListActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/dynamic/detail/dig_list/DigListContract.java
@@ -0,0 +1,32 @@
+package com.zhiyicx.thinksnsplus.modules.dynamic.detail.dig_list;
+
+import com.zhiyicx.baseproject.base.ITSListPresenter;
+import com.zhiyicx.baseproject.base.ITSListView;
+import com.zhiyicx.thinksnsplus.data.beans.DynamicBean;
+import com.zhiyicx.thinksnsplus.data.beans.DynamicToolBean;
+import com.zhiyicx.thinksnsplus.data.beans.UserInfoBean;
+import com.zhiyicx.thinksnsplus.modules.dynamic.IDynamicReppsitory;
+import com.zhiyicx.thinksnsplus.modules.dynamic.detail.DynamicDetailContract;
+
+import java.util.List;
+
+/**
+ * @author LiuChao
+ * @describe
+ * @date 2017/3/3
+ * @contact email:450127106@qq.com
+ */
+
+public interface DigListContract {
+    //对于经常使用的关于UI的方法可以定义到BaseView中,如显示隐藏进度条,和显示文字消息
+    interface View extends ITSListView<DynamicBean, DigListContract.Presenter> {
+    }
+
+    //Model层定义接口,外部只需关心model返回的数据,无需关心内部细节,及是否使用缓存
+    interface Repository {
+
+    }
+
+    interface Presenter extends ITSListPresenter<DynamicBean> {
+    }
+}
