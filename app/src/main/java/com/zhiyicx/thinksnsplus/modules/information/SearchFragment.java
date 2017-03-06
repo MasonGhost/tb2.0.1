6fa43b7d3ccd5c067766ecbe4f0d7a76f77dd282
Jliuer <jliuer@aliyun.com>
2017-03-06 18:33:07 +0800

    (#105) code 搜索界面

---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/information/SearchFragment.java
@@ -0,0 +1,35 @@
+package com.zhiyicx.thinksnsplus.modules.information;
+
+import android.view.View;
+
+import com.zhiyicx.baseproject.base.TSFragment;
+import com.zhiyicx.thinksnsplus.R;
+
+/**
+ * @Author Jliuer
+ * @Date 2017/03/06
+ * @Email Jliuer@aliyun.com
+ * @Description
+ */
+public class SearchFragment extends TSFragment {
+
+    @Override
+    protected int getBodyLayoutId() {
+        return R.layout.fragment_info_search;
+    }
+
+    @Override
+    protected void initView(View rootView) {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected boolean showToolbar() {
+        return false;
+    }
+}
