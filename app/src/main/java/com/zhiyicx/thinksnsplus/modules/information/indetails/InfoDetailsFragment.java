8b61f1a47beb251f38eaa1eadd1b8ec96c0c9fb3
Jliuer <jliuer@aliyun.com>
2017-03-07 18:20:48 +0800

    #(105) code 资讯详情界面

--- app/src/main/java/com/zhiyicx/thinksnsplus/modules/information/indetails/InfoDetailsFragment.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/information/indetails/InfoDetailsFragment.java
@@ -1,8 +1,22 @@
 package com.zhiyicx.thinksnsplus.modules.information.indetails;
 
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
+import com.aspsine.swipetoloadlayout.SwipeToLoadLayout;
 import com.zhiyicx.baseproject.base.TSFragment;
+import com.zhiyicx.baseproject.base.TSListFragment;
+import com.zhiyicx.baseproject.widget.DynamicDetailMenuView;
+import com.zhiyicx.baseproject.widget.pictureviewer.PictureViewer;
+import com.zhiyicx.thinksnsplus.R;
+import com.zhy.adapter.recyclerview.MultiItemTypeAdapter;
+
+import butterknife.BindView;
 
 /**
  * @Author Jliuer
@@ -10,19 +24,53 @@ import com.zhiyicx.baseproject.base.TSFragment;
  * @Email Jliuer@aliyun.com
  * @Description
  */
-public class InfoDetailsFragment extends TSFragment {
+public class InfoDetailsFragment extends TSListFragment {
+
+    @BindView(R.id.tv_top_tip_text)
+    TextView mTvTopTipText;
+    @BindView(R.id.fl_top_tip_container)
+    FrameLayout mFlTopTipContainer;
+    @BindView(R.id.swipe_target)
+    RecyclerView mSwipeTarget;
+    @BindView(R.id.refreshlayout)
+    SwipeToLoadLayout mRefreshlayout;
+    @BindView(R.id.picture_view)
+    PictureViewer mPictureView;
+    @BindView(R.id.dd_dynamic_tool)
+    DynamicDetailMenuView mDdDynamicTool;
+
+    @Override
+    protected String setCenterTitle() {
+        return getString(R.string.info_details);
+    }
+
     @Override
     protected int getBodyLayoutId() {
-        return 0;
+        return R.layout.fragment_info_detail;
     }
 
     @Override
-    protected void initView(View rootView) {
+    protected MultiItemTypeAdapter getAdapter() {
+        return null;
+    }
+
+    @Override
+    public void setPresenter(Object presenter) {
 
     }
 
     @Override
-    protected void initData() {
+    public void showLoading() {
+
+    }
+
+    @Override
+    public void hideLoading() {
+
+    }
+
+    @Override
+    public void showMessage(String message) {
 
     }
 }
--- app/src/main/java/com/zhiyicx/thinksnsplus/modules/information/infochannel/ChannelActivity.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/information/infochannel/ChannelActivity.java
@@ -3,7 +3,6 @@ package com.zhiyicx.thinksnsplus.modules.information.infochannel;
 import android.support.v4.app.Fragment;
 
 import com.zhiyicx.baseproject.base.TSActivity;
-import com.zhiyicx.thinksnsplus.modules.information.InfoChannelFragment;
 
 /**
  * @Author Jliuer
