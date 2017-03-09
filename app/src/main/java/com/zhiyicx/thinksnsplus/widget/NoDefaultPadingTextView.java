47291c311b8b7b4b14884c7f87631240a096cb38
Jliuer <jliuer@aliyun.com>
2017-03-09 18:10:30 +0800

    (#105) code 取消textview的默认间距

---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/widget/NoDefaultPadingTextView.java
@@ -0,0 +1,12 @@
+package com.zhiyicx.thinksnsplus.widget;
+
+import android.widget.TextView;
+
+/**
+ * @Author Jliuer
+ * @Date 2017/03/09
+ * @Email Jliuer@aliyun.com
+ * @Description
+ */
+public class NoDefaultPadingTextView extends TextView {
+}
