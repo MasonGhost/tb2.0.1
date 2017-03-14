ed1c17afed7337e66dad7b5c1b5a2fe2e88adcce
LC\ZYCX <450127106@qq.com>
2017-03-14 11:03:24 +0800

    （#64）code 修改缺省图

--- PhotoPicker/src/main/java/me/iwf/photopicker/adapter/PhotoGridAdapter.java
+++ PhotoPicker/src/main/java/me/iwf/photopicker/adapter/PhotoGridAdapter.java
@@ -12,6 +12,7 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -120,9 +121,11 @@ public class PhotoGridAdapter extends SelectableAdapter<PhotoGridAdapter.PhotoVi
                         .centerCrop()
                         .dontAnimate()
                         .thumbnail(0.5f)
+                        .skipMemoryCache(true)
+                        .diskCacheStrategy(DiskCacheStrategy.NONE)
                         .override(imageSize, imageSize)
-                        .placeholder(R.drawable.__picker_ic_photo_black_48dp)
-                        .error(R.drawable.__picker_ic_broken_image_black_48dp)
+                        .placeholder(R.drawable.__picker_shape_default_image)
+                        .error(R.drawable.__picker_shape_default_image)
                         .into(holder.ivPhoto);
             }
 
