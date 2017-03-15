67cb15c1c34b9f029fbeced209377bc33271edcb
Jliuer <jliuer@aliyun.com>
2017-03-15 22:18:33 +0800

    (#75) code 音乐专辑列表、音乐专辑详情实体类

--- app/src/main/java/com/zhiyicx/thinksnsplus/data/beans/MusicAlbumDetailsBean.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/data/beans/MusicAlbumDetailsBean.java
@@ -3,6 +3,7 @@ package com.zhiyicx.thinksnsplus.data.beans;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,146 +15,242 @@ import java.util.List;
 public class MusicAlbumDetailsBean implements Parcelable {
 
     /**
-     * status : true
-     * code : 0
-     * message : 操作成功
-     * data : {"info":"简介","taste_count":1000,"share_count":1000,"comment_count":1000,
-     * "collect_count":1000,"music":[{"music_id":1,"title":"七里香","singer":"周杰伦","storage":3},
-     * {"music_id":2,"title":"如果这都不算爱","singer":"张学友","storage":4}]}
+     * id : 1
+     * created_at : 2017-03-10 18:05:02
+     * updated_at : 2017-03-10 18:05:03
+     * title : 专辑1
+     * storage : 1
+     * taste_count : 0
+     * share_count : 0
+     * comment_count : 0
+     * collect_count : 0
+     * musics : [{"id":1,"created_at":"2017-03-10 18:05:15","updated_at":"2017-03-10 18:05:16",
+     * "special_id":"1","music_id":1,"music_info":{"id":1,"created_at":"2017-03-10 18:05:22",
+     * "updated_at":"2017-03-10 18:05:23","deleted_at":null,"title":"音乐1","singer":1,"storage":2,
+     * "last_time":180,"lyric":"啦啦啦啦啦啦","taste_count":0,"share_count":0,"comment_count":0}},
+     * {"id":2,"created_at":"2017-03-10 18:05:15","updated_at":"2017-03-10 18:05:16",
+     * "special_id":"1","music_id":2,"music_info":{"id":2,"created_at":"2017-03-10 18:05:22",
+     * "updated_at":"2017-03-10 18:05:23","deleted_at":null,"title":"音乐2","singer":1,"storage":2,
+     * "last_time":180,"lyric":"啦啦啦啦啦啦","taste_count":0,"share_count":0,"comment_count":0}}]
      */
 
-    private boolean status;
-    private int code;
-    private String message;
-    private DataBean data;
+    private int id;
+    private String created_at;
+    private String updated_at;
+    private String title;
+    private int storage;
+    private int taste_count;
+    private int share_count;
+    private int comment_count;
+    private int collect_count;
+    private List<MusicsBean> musics;
 
-    public boolean isStatus() {
-        return status;
+    public int getId() {
+        return id;
     }
 
-    public void setStatus(boolean status) {
-        this.status = status;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public int getCode() {
-        return code;
+    public String getCreated_at() {
+        return created_at;
     }
 
-    public void setCode(int code) {
-        this.code = code;
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
     }
 
-    public String getMessage() {
-        return message;
+    public String getUpdated_at() {
+        return updated_at;
     }
 
-    public void setMessage(String message) {
-        this.message = message;
+    public void setUpdated_at(String updated_at) {
+        this.updated_at = updated_at;
     }
 
-    public DataBean getData() {
-        return data;
+    public String getTitle() {
+        return title;
     }
 
-    public void setData(DataBean data) {
-        this.data = data;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
-    public static class DataBean implements Parcelable {
+    public int getStorage() {
+        return storage;
+    }
+
+    public void setStorage(int storage) {
+        this.storage = storage;
+    }
+
+    public int getTaste_count() {
+        return taste_count;
+    }
+
+    public void setTaste_count(int taste_count) {
+        this.taste_count = taste_count;
+    }
+
+    public int getShare_count() {
+        return share_count;
+    }
+
+    public void setShare_count(int share_count) {
+        this.share_count = share_count;
+    }
+
+    public int getComment_count() {
+        return comment_count;
+    }
+
+    public void setComment_count(int comment_count) {
+        this.comment_count = comment_count;
+    }
+
+    public int getCollect_count() {
+        return collect_count;
+    }
+
+    public void setCollect_count(int collect_count) {
+        this.collect_count = collect_count;
+    }
+
+    public List<MusicsBean> getMusics() {
+        return musics;
+    }
+
+    public void setMusics(List<MusicsBean> musics) {
+        this.musics = musics;
+    }
+
+    public static class MusicsBean {
         /**
-         * info : 简介
-         * taste_count : 1000
-         * share_count : 1000
-         * comment_count : 1000
-         * collect_count : 1000
-         * music : [{"music_id":1,"title":"七里香","singer":"周杰伦","storage":3},{"music_id":2,
-         * "title":"如果这都不算爱","singer":"张学友","storage":4}]
-         * <p>
-         * info string 简介
-         * taste_count int 播放数量
-         * share_count int 转发数量
-         * comment_count int 评论数量
-         * collect_count int 收藏数量
-         * music_id int 歌曲id
-         * title string 歌曲名称
-         * singer string 周杰伦
-         * storage int 歌曲附件id
+         * id : 1
+         * created_at : 2017-03-10 18:05:15
+         * updated_at : 2017-03-10 18:05:16
+         * special_id : 1
+         * music_id : 1
+         * music_info : {"id":1,"created_at":"2017-03-10 18:05:22","updated_at":"2017-03-10
+         * 18:05:23","deleted_at":null,"title":"音乐1","singer":1,"storage":2,"last_time":180,
+         * "lyric":"啦啦啦啦啦啦","taste_count":0,"share_count":0,"comment_count":0}
          */
 
-        private String info;
-        private int taste_count;
-        private int share_count;
-        private int comment_count;
-        private int collect_count;
-        private List<MusicBean> music;
+        private int id;
+        private String created_at;
+        private String updated_at;
+        private String special_id;
+        private int music_id;
+        private MusicInfoBean music_info;
 
-        public String getInfo() {
-            return info;
+        public int getId() {
+            return id;
         }
 
-        public void setInfo(String info) {
-            this.info = info;
+        public void setId(int id) {
+            this.id = id;
         }
 
-        public int getTaste_count() {
-            return taste_count;
+        public String getCreated_at() {
+            return created_at;
         }
 
-        public void setTaste_count(int taste_count) {
-            this.taste_count = taste_count;
+        public void setCreated_at(String created_at) {
+            this.created_at = created_at;
         }
 
-        public int getShare_count() {
-            return share_count;
+        public String getUpdated_at() {
+            return updated_at;
         }
 
-        public void setShare_count(int share_count) {
-            this.share_count = share_count;
+        public void setUpdated_at(String updated_at) {
+            this.updated_at = updated_at;
         }
 
-        public int getComment_count() {
-            return comment_count;
+        public String getSpecial_id() {
+            return special_id;
         }
 
-        public void setComment_count(int comment_count) {
-            this.comment_count = comment_count;
+        public void setSpecial_id(String special_id) {
+            this.special_id = special_id;
         }
 
-        public int getCollect_count() {
-            return collect_count;
+        public int getMusic_id() {
+            return music_id;
         }
 
-        public void setCollect_count(int collect_count) {
-            this.collect_count = collect_count;
+        public void setMusic_id(int music_id) {
+            this.music_id = music_id;
         }
 
-        public List<MusicBean> getMusic() {
-            return music;
+        public MusicInfoBean getMusic_info() {
+            return music_info;
         }
 
-        public void setMusic(List<MusicBean> music) {
-            this.music = music;
+        public void setMusic_info(MusicInfoBean music_info) {
+            this.music_info = music_info;
         }
 
-        public static class MusicBean implements Parcelable {
+        public static class MusicInfoBean {
             /**
-             * music_id : 1
-             * title : 七里香
-             * singer : 周杰伦
-             * storage : 3
+             * id : 1
+             * created_at : 2017-03-10 18:05:22
+             * updated_at : 2017-03-10 18:05:23
+             * deleted_at : null
+             * title : 音乐1
+             * singer : 1
+             * storage : 2
+             * last_time : 180
+             * lyric : 啦啦啦啦啦啦
+             * taste_count : 0
+             * share_count : 0
+             * comment_count : 0
              */
 
-            private int music_id;
+            private int id;
+            private String created_at;
+            private String updated_at;
+            private Object deleted_at;
             private String title;
-            private String singer;
+            private int singer;
             private int storage;
+            private int last_time;
+            private String lyric;
+            private int taste_count;
+            private int share_count;
+            private int comment_count;
 
-            public int getMusic_id() {
-                return music_id;
+            public int getId() {
+                return id;
             }
 
-            public void setMusic_id(int music_id) {
-                this.music_id = music_id;
+            public void setId(int id) {
+                this.id = id;
+            }
+
+            public String getCreated_at() {
+                return created_at;
+            }
+
+            public void setCreated_at(String created_at) {
+                this.created_at = created_at;
+            }
+
+            public String getUpdated_at() {
+                return updated_at;
+            }
+
+            public void setUpdated_at(String updated_at) {
+                this.updated_at = updated_at;
+            }
+
+            public Object getDeleted_at() {
+                return deleted_at;
+            }
+
+            public void setDeleted_at(Object deleted_at) {
+                this.deleted_at = deleted_at;
             }
 
             public String getTitle() {
@@ -164,11 +261,11 @@ public class MusicAlbumDetailsBean implements Parcelable {
                 this.title = title;
             }
 
-            public String getSinger() {
+            public int getSinger() {
                 return singer;
             }
 
-            public void setSinger(String singer) {
+            public void setSinger(int singer) {
                 this.singer = singer;
             }
 
@@ -180,80 +277,46 @@ public class MusicAlbumDetailsBean implements Parcelable {
                 this.storage = storage;
             }
 
-            @Override
-            public int describeContents() {
-                return 0;
+            public int getLast_time() {
+                return last_time;
             }
 
-            @Override
-            public void writeToParcel(Parcel dest, int flags) {
-                dest.writeInt(this.music_id);
-                dest.writeString(this.title);
-                dest.writeString(this.singer);
-                dest.writeInt(this.storage);
+            public void setLast_time(int last_time) {
+                this.last_time = last_time;
             }
 
-            public MusicBean() {
+            public String getLyric() {
+                return lyric;
             }
 
-            protected MusicBean(Parcel in) {
-                this.music_id = in.readInt();
-                this.title = in.readString();
-                this.singer = in.readString();
-                this.storage = in.readInt();
+            public void setLyric(String lyric) {
+                this.lyric = lyric;
             }
 
-            public static final Parcelable.Creator<MusicBean> CREATOR = new Parcelable.Creator<MusicBean>() {
-                @Override
-                public MusicBean createFromParcel(Parcel source) {
-                    return new MusicBean(source);
-                }
+            public int getTaste_count() {
+                return taste_count;
+            }
 
-                @Override
-                public MusicBean[] newArray(int size) {
-                    return new MusicBean[size];
-                }
-            };
+            public void setTaste_count(int taste_count) {
+                this.taste_count = taste_count;
+            }
+
+            public int getShare_count() {
+                return share_count;
+            }
+
+            public void setShare_count(int share_count) {
+                this.share_count = share_count;
+            }
+
+            public int getComment_count() {
+                return comment_count;
+            }
+
+            public void setComment_count(int comment_count) {
+                this.comment_count = comment_count;
+            }
         }
-
-        @Override
-        public int describeContents() {
-            return 0;
-        }
-
-        @Override
-        public void writeToParcel(Parcel dest, int flags) {
-            dest.writeString(this.info);
-            dest.writeInt(this.taste_count);
-            dest.writeInt(this.share_count);
-            dest.writeInt(this.comment_count);
-            dest.writeInt(this.collect_count);
-            dest.writeTypedList(this.music);
-        }
-
-        public DataBean() {
-        }
-
-        protected DataBean(Parcel in) {
-            this.info = in.readString();
-            this.taste_count = in.readInt();
-            this.share_count = in.readInt();
-            this.comment_count = in.readInt();
-            this.collect_count = in.readInt();
-            this.music = in.createTypedArrayList(MusicBean.CREATOR);
-        }
-
-        public static final Parcelable.Creator<DataBean> CREATOR = new Parcelable.Creator<DataBean>() {
-            @Override
-            public DataBean createFromParcel(Parcel source) {
-                return new DataBean(source);
-            }
-
-            @Override
-            public DataBean[] newArray(int size) {
-                return new DataBean[size];
-            }
-        };
     }
 
     @Override
@@ -263,24 +326,36 @@ public class MusicAlbumDetailsBean implements Parcelable {
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
-        dest.writeByte(this.status ? (byte) 1 : (byte) 0);
-        dest.writeInt(this.code);
-        dest.writeString(this.message);
-        dest.writeParcelable(this.data, flags);
+        dest.writeInt(this.id);
+        dest.writeString(this.created_at);
+        dest.writeString(this.updated_at);
+        dest.writeString(this.title);
+        dest.writeInt(this.storage);
+        dest.writeInt(this.taste_count);
+        dest.writeInt(this.share_count);
+        dest.writeInt(this.comment_count);
+        dest.writeInt(this.collect_count);
+        dest.writeList(this.musics);
     }
 
     public MusicAlbumDetailsBean() {
     }
 
     protected MusicAlbumDetailsBean(Parcel in) {
-        this.status = in.readByte() != 0;
-        this.code = in.readInt();
-        this.message = in.readString();
-        this.data = in.readParcelable(DataBean.class.getClassLoader());
+        this.id = in.readInt();
+        this.created_at = in.readString();
+        this.updated_at = in.readString();
+        this.title = in.readString();
+        this.storage = in.readInt();
+        this.taste_count = in.readInt();
+        this.share_count = in.readInt();
+        this.comment_count = in.readInt();
+        this.collect_count = in.readInt();
+        this.musics = new ArrayList<MusicsBean>();
+        in.readList(this.musics, MusicsBean.class.getClassLoader());
     }
 
-    public static final Parcelable.Creator<MusicAlbumDetailsBean> CREATOR = new Parcelable
-            .Creator<MusicAlbumDetailsBean>() {
+    public static final Creator<MusicAlbumDetailsBean> CREATOR = new Creator<MusicAlbumDetailsBean>() {
         @Override
         public MusicAlbumDetailsBean createFromParcel(Parcel source) {
             return new MusicAlbumDetailsBean(source);
--- app/src/main/java/com/zhiyicx/thinksnsplus/data/beans/MusicAlbumListBean.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/data/beans/MusicAlbumListBean.java
@@ -6,111 +6,175 @@ import android.os.Parcelable;
 
 import com.zhiyicx.baseproject.base.BaseListBean;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @Author Jliuer
  * @Date 2017/02/13
  * @Email Jliuer@aliyun.com
  * @Description 专辑列表
  */
-public class MusicAlbumListBean extends BaseListBean implements Parcelable {
+public class MusicAlbumListBean extends BaseListBean{
 
     /**
-     * status : true
-     * code : 0
-     * message : 操作成功
-     * data : [{"sheet_id":1,"title":"周杰伦","storage":1,"taste_count":2000},{"sheet_id":2,
-     * "title":"陈奕迅","storage":2,"taste_count":1000}]
+     * id : 2
+     * created_at : 2017-03-15 17:04:31
+     * updated_at : 2017-03-15 17:04:34
+     * title : 专辑2
+     * storage : {"id":5,"image_width":1080,"image_height":1800}
+     * taste_count : 0
+     * share_count : 0
+     * comment_count : 0
+     * collect_count : 0
      */
 
-    private boolean status;
-    private int code;
-    private String message;
-    private List<DataBean> data;
+    private int id;
+    private String created_at;
+    private String updated_at;
+    private String title;
+    private StorageBean storage;
+    private int taste_count;
+    private int share_count;
+    private int comment_count;
+    private int collect_count;
 
-    public boolean isStatus() {
-        return status;
+    public int getId() {
+        return id;
     }
 
-    public void setStatus(boolean status) {
-        this.status = status;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public int getCode() {
-        return code;
+    public String getCreated_at() {
+        return created_at;
     }
 
-    public void setCode(int code) {
-        this.code = code;
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
     }
 
-    public String getMessage() {
-        return message;
+    public String getUpdated_at() {
+        return updated_at;
     }
 
-    public void setMessage(String message) {
-        this.message = message;
+    public void setUpdated_at(String updated_at) {
+        this.updated_at = updated_at;
     }
 
-    public List<DataBean> getData() {
-        return data;
+    public String getTitle() {
+        return title;
     }
 
-    public void setData(List<DataBean> data) {
-        this.data = data;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
-    public static class DataBean {
+    public StorageBean getStorage() {
+        return storage;
+    }
+
+    public void setStorage(StorageBean storage) {
+        this.storage = storage;
+    }
+
+    public int getTaste_count() {
+        return taste_count;
+    }
+
+    public void setTaste_count(int taste_count) {
+        this.taste_count = taste_count;
+    }
+
+    public int getShare_count() {
+        return share_count;
+    }
+
+    public void setShare_count(int share_count) {
+        this.share_count = share_count;
+    }
+
+    public int getComment_count() {
+        return comment_count;
+    }
+
+    public void setComment_count(int comment_count) {
+        this.comment_count = comment_count;
+    }
+
+    public int getCollect_count() {
+        return collect_count;
+    }
+
+    public void setCollect_count(int collect_count) {
+        this.collect_count = collect_count;
+    }
+
+    public static class StorageBean implements Parcelable{
         /**
-         * sheet_id : 1
-         * title : 周杰伦
-         * storage : 1
-         * taste_count : 2000
-         * <p>
-         * sheet_id int 专辑id
-         * title 专辑名称
-         * storage int 封面id
-         * taste_count int 播放数量
+         * id : 5
+         * image_width : 1080
+         * image_height : 1800
          */
 
-        private int sheet_id;
-        private String title;
-        private int storage;
-        private int taste_count;
+        private int id;
+        private int image_width;
+        private int image_height;
 
-        public int getSheet_id() {
-            return sheet_id;
+        public int getId() {
+            return id;
         }
 
-        public void setSheet_id(int sheet_id) {
-            this.sheet_id = sheet_id;
+        public void setId(int id) {
+            this.id = id;
         }
 
-        public String getTitle() {
-            return title;
+        public int getImage_width() {
+            return image_width;
         }
 
-        public void setTitle(String title) {
-            this.title = title;
+        public void setImage_width(int image_width) {
+            this.image_width = image_width;
         }
 
-        public int getStorage() {
-            return storage;
+        public int getImage_height() {
+            return image_height;
         }
 
-        public void setStorage(int storage) {
-            this.storage = storage;
+        public void setImage_height(int image_height) {
+            this.image_height = image_height;
         }
 
-        public int getTaste_count() {
-            return taste_count;
+        @Override
+        public int describeContents() {
+            return 0;
         }
 
-        public void setTaste_count(int taste_count) {
-            this.taste_count = taste_count;
+        @Override
+        public void writeToParcel(Parcel dest, int flags) {
+            dest.writeInt(this.id);
+            dest.writeInt(this.image_width);
+            dest.writeInt(this.image_height);
         }
+
+        public StorageBean() {
+        }
+
+        protected StorageBean(Parcel in) {
+            this.id = in.readInt();
+            this.image_width = in.readInt();
+            this.image_height = in.readInt();
+        }
+
+        public static final Creator<StorageBean> CREATOR = new Creator<StorageBean>() {
+            @Override
+            public StorageBean createFromParcel(Parcel source) {
+                return new StorageBean(source);
+            }
+
+            @Override
+            public StorageBean[] newArray(int size) {
+                return new StorageBean[size];
+            }
+        };
     }
 
     @Override
@@ -121,10 +185,15 @@ public class MusicAlbumListBean extends BaseListBean implements Parcelable {
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         super.writeToParcel(dest, flags);
-        dest.writeByte(this.status ? (byte) 1 : (byte) 0);
-        dest.writeInt(this.code);
-        dest.writeString(this.message);
-        dest.writeList(this.data);
+        dest.writeInt(this.id);
+        dest.writeString(this.created_at);
+        dest.writeString(this.updated_at);
+        dest.writeString(this.title);
+        dest.writeParcelable(this.storage, flags);
+        dest.writeInt(this.taste_count);
+        dest.writeInt(this.share_count);
+        dest.writeInt(this.comment_count);
+        dest.writeInt(this.collect_count);
     }
 
     public MusicAlbumListBean() {
@@ -132,11 +201,15 @@ public class MusicAlbumListBean extends BaseListBean implements Parcelable {
 
     protected MusicAlbumListBean(Parcel in) {
         super(in);
-        this.status = in.readByte() != 0;
-        this.code = in.readInt();
-        this.message = in.readString();
-        this.data = new ArrayList<DataBean>();
-        in.readList(this.data, DataBean.class.getClassLoader());
+        this.id = in.readInt();
+        this.created_at = in.readString();
+        this.updated_at = in.readString();
+        this.title = in.readString();
+        this.storage = in.readParcelable(StorageBean.class.getClassLoader());
+        this.taste_count = in.readInt();
+        this.share_count = in.readInt();
+        this.comment_count = in.readInt();
+        this.collect_count = in.readInt();
     }
 
     public static final Creator<MusicAlbumListBean> CREATOR = new Creator<MusicAlbumListBean>() {
