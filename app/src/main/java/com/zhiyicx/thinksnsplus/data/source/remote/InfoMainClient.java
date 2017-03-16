dda8c106bf58e0384b859405a4c2af9916bd8e7c
Jliuer <jliuer@aliyun.com>
2017-03-16 17:08:51 +0800

    (#109) code 资讯接口

--- app/src/main/java/com/zhiyicx/thinksnsplus/data/source/remote/InfoMainClient.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/data/source/remote/InfoMainClient.java
@@ -6,13 +6,18 @@ import com.zhiyicx.thinksnsplus.data.beans.InfoTypeBean;
 
 import java.util.List;
 
+import retrofit2.http.DELETE;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 import rx.Observable;
 
+import static com.zhiyicx.baseproject.config.ApiConfig.APP_PATH_INFO_COLLECT;
+import static com.zhiyicx.baseproject.config.ApiConfig.APP_PATH_INFO_COMMENT;
+import static com.zhiyicx.baseproject.config.ApiConfig.APP_PATH_INFO_COMMENT_LIST;
 import static com.zhiyicx.baseproject.config.ApiConfig.APP_PATH_INFO_FOLLOW_LIST;
 import static com.zhiyicx.baseproject.config.ApiConfig.APP_PATH_INFO_LIST;
 import static com.zhiyicx.baseproject.config.ApiConfig.APP_PATH_INFO_TYPE;
@@ -26,15 +31,48 @@ import static com.zhiyicx.baseproject.config.ApiConfig.APP_PATH_LOGIN;
  */
 public interface InfoMainClient {
 
+    // 获取资讯分类
     @GET(APP_PATH_INFO_TYPE)
     Observable<BaseJson<InfoTypeBean>> getInfoType();
 
+    // 获取某类资讯列表
     @GET(APP_PATH_INFO_LIST)
-    Observable<BaseJson<List<InfoListBean>>> getInfoList(@Query("cate_id") String cate_id,
+    Observable<BaseJson<InfoListBean>> getInfoList(@Query("cate_id") String cate_id,
                                                          @Query("max_id") Long max_id,
                                                          @Query("limit") Long limit,
                                                          @Query("page") Long page);
+
+    // 订阅某类资讯
     @FormUrlEncoded
     @POST(APP_PATH_INFO_FOLLOW_LIST)
     Observable<BaseJson<Integer>> doSubscribe(@Field("follows") String follows);
+
+    // 收藏资讯
+    @FormUrlEncoded
+    @POST(APP_PATH_INFO_COLLECT)
+    Observable<BaseJson<Integer>> collectInfo(@Path("news_id") String news_id);
+
+    // 取消收藏资讯
+    @FormUrlEncoded
+    @DELETE(APP_PATH_INFO_COLLECT)
+    Observable<BaseJson<Integer>> cancleCollectInfo(@Path("news_id") String news_id);
+
+    // 获取一条资讯的评论列表
+    @GET(APP_PATH_INFO_COMMENT_LIST)
+    Observable<BaseJson<InfoListBean>> getInfoCommentList(@Path("feed_id") String feed_id,
+                                                   @Query("max_id") Long max_id,
+                                                   @Query("limit") Long limit);
+
+    /**
+     * 对一条资讯或一条资讯评论进行评论
+     * @param comment_content 内容
+     * @param reply_to_user_id 被评论者id 对评论进行评论时传入
+     * @return
+     */
+    @FormUrlEncoded
+    @POST(APP_PATH_INFO_COMMENT)
+    Observable<BaseJson<Integer>> commentInfo(@Field("comment_content") String comment_content,
+                                              @Field("reply_to_user_id") int reply_to_user_id);
+
+
 }
