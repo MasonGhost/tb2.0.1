db430daafbc26d11d0e3f17411101d4b8e598620
Jliuer <jliuer@aliyun.com>
2017-03-16 17:07:26 +0800

    (#109) code 新增音乐评论列表、专辑评论列表、专辑信息实体类

---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/data/beans/AblumCommentListBean.java
@@ -0,0 +1,12 @@
+package com.zhiyicx.thinksnsplus.data.beans;
+
+import com.zhiyicx.baseproject.base.BaseListBean;
+
+/**
+ * @Author Jliuer
+ * @Date 2017/03/16
+ * @Email Jliuer@aliyun.com
+ * @Description
+ */
+public class AblumCommentListBean extends BaseListBean {
+}
---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/music_fm/media_data/MusicAblumInfo.java
@@ -0,0 +1,10 @@
+package com.zhiyicx.thinksnsplus.modules.music_fm.media_data;
+
+/**
+ * @Author Jliuer
+ * @Date 2017/03/16
+ * @Email Jliuer@aliyun.com
+ * @Description
+ */
+public class MusicAblumInfo implements MusicProviderSource {
+}
