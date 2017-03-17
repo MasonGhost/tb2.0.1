af3ac2aaa3a9f041d6ece17d5c3c1ddd5349e903
Jungle68 <335891510@qq.com>
2017-03-17 10:16:11 +0800

    (#87) test 忽略音乐详情测试

--- app/src/androidTest/java/com/zhiyicx/thinksnsplus/modules/music_fm/music_album_detail/MusicAlbumDetailFragmentTest.java
+++ app/src/androidTest/java/com/zhiyicx/thinksnsplus/modules/music_fm/music_album_detail/MusicAlbumDetailFragmentTest.java
@@ -1,13 +1,20 @@
 package com.zhiyicx.thinksnsplus.modules.music_fm.music_album_detail;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 
+import com.zhiyicx.common.utils.TimeUtils;
 import com.zhiyicx.thinksnsplus.R;
-import com.zhiyicx.thinksnsplus.modules.music_fm.music_play.MusicPlayActivity;
+import com.zhiyicx.thinksnsplus.data.beans.MusicAlbumListBean;
+import com.zhiyicx.thinksnsplus.modules.music_fm.music_album.MusicListFragment;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -26,8 +33,26 @@ public class MusicAlbumDetailFragmentTest {
     private ViewInteraction music_list, bt_goto;
 
     @Rule
-    public ActivityTestRule<MusicDetailActivity> mActivityRule = new ActivityTestRule
-            (MusicDetailActivity.class);
+    public ActivityTestRule<MusicDetailActivity> mActivityRule =
+            new ActivityTestRule<MusicDetailActivity>(MusicDetailActivity.class) {
+                @Override
+                protected Intent getActivityIntent() {
+                    Context targetContext = InstrumentationRegistry.getInstrumentation()
+                            .getTargetContext();
+                    Intent result = new Intent(targetContext, MusicDetailActivity.class);
+                    Bundle bundle = new Bundle();
+                    MusicAlbumListBean messageItemBean = new MusicAlbumListBean();
+                    messageItemBean.setCreated_at(TimeUtils.millis2String(System.currentTimeMillis()));
+                    messageItemBean.setCollect_count(5);
+                    messageItemBean.setStorage(new MusicAlbumListBean.StorageBean());
+                    messageItemBean.setTitle("test");
+                    messageItemBean.setId(100);
+                    messageItemBean.setMaxId(System.currentTimeMillis());
+                    bundle.putParcelable(MusicListFragment.BUNDLE_MUSIC_ABLUM, messageItemBean);
+                    result.putExtra(MusicListFragment.BUNDLE_MUSIC_ABLUM, bundle);
+                    return result;
+                }
+            };
 
     @Before
     public void initActivity() {
@@ -35,6 +60,7 @@ public class MusicAlbumDetailFragmentTest {
         bt_goto = onView(withId(R.id.fragment_back_content));
     }
 
+    @Ignore
     @Test
     public void playMusic() {
         music_list.perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
