365504753c5cb5c988c01337832f98c0099e0243
Jliuer <jliuer@aliyun.com>
2017-03-21 18:15:45 +0800

    (#75) code 音乐播放管理

--- app/src/main/java/com/zhiyicx/thinksnsplus/modules/music_fm/media_data/MusicAblumInfo.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/music_fm/media_data/MusicAblumInfo.java
@@ -2,6 +2,7 @@ package com.zhiyicx.thinksnsplus.modules.music_fm.media_data;
 
 import android.support.v4.media.MediaMetadataCompat;
 
+import com.zhiyicx.baseproject.config.ApiConfig;
 import com.zhiyicx.common.utils.log.LogUtils;
 import com.zhiyicx.thinksnsplus.data.beans.MusicAlbumDetailsBean;
 
@@ -37,17 +38,22 @@ public class MusicAblumInfo implements MusicProviderSource {
 
     private MediaMetadataCompat buildMusic(MusicAlbumDetailsBean.MusicsBean data) {
         MusicAlbumDetailsBean.MusicsBean.MusicInfoBean needData=data.getMusic_info();
+        String musicUrl = String.format(ApiConfig.NO_PROCESS_IMAGE_PATH,
+                needData.getStorage());
+
+        String imageUrl = String.format(ApiConfig.NO_PROCESS_IMAGE_PATH,
+                needData.getSinger().getCover().getId(),50);
         //noinspection ResourceType
         return new MediaMetadataCompat.Builder()
                 .putString(MediaMetadataCompat.METADATA_KEY_MEDIA_ID,
                         ""+needData.getId())
                 .putString(MusicProviderSource.CUSTOM_METADATA_TRACK_SOURCE,
-                        ""+needData.getStorage())
-                .putString(MediaMetadataCompat.METADATA_KEY_ALBUM, ""+needData.getStorage())
-                .putString(MediaMetadataCompat.METADATA_KEY_ARTIST, needData.getSinger()+"")
+                        ""+musicUrl)
+                .putString(MediaMetadataCompat.METADATA_KEY_ALBUM, ""+needData.getTitle())
+                .putString(MediaMetadataCompat.METADATA_KEY_ARTIST, needData.getSinger().getName())
                 .putLong(MediaMetadataCompat.METADATA_KEY_DURATION, needData.getLast_time()*1000)
-                .putString(MediaMetadataCompat.METADATA_KEY_GENRE, needData.getTitle())
-                .putString(MediaMetadataCompat.METADATA_KEY_ALBUM_ART_URI, ""+needData.getStorage())
+                .putString(MediaMetadataCompat.METADATA_KEY_GENRE, "tym_test")
+                .putString(MediaMetadataCompat.METADATA_KEY_ALBUM_ART_URI, imageUrl)
                 .putString(MediaMetadataCompat.METADATA_KEY_TITLE, needData.getTitle())
                 .build();
     }
