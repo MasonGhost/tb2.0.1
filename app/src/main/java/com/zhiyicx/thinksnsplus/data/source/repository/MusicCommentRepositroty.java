b207405a33fb52bcda8c29be7307f1f3ff617e6b
Jliuer <jliuer@aliyun.com>
2017-03-22 17:51:18 +0800

    (#75) code 新增 【音乐评论】 音乐评论相关结构

---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/data/source/repository/MusicCommentRepositroty.java
@@ -0,0 +1,12 @@
+package com.zhiyicx.thinksnsplus.data.source.repository;
+
+import com.zhiyicx.thinksnsplus.modules.music_fm.music_comment.MusicCommentContract;
+
+/**
+ * @Author Jliuer
+ * @Date 2017/03/22
+ * @Email Jliuer@aliyun.com
+ * @Description
+ */
+public class MusicCommentRepositroty implements MusicCommentContract.Repository {
+}
---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/music_fm/music_comment/MusicCommentPresenter.java
@@ -0,0 +1,10 @@
+package com.zhiyicx.thinksnsplus.modules.music_fm.music_comment;
+
+/**
+ * @Author Jliuer
+ * @Date 2017/03/22
+ * @Email Jliuer@aliyun.com
+ * @Description
+ */
+public class MusicCommentPresenter implements MusicCommentContract.Presenter {
+}
