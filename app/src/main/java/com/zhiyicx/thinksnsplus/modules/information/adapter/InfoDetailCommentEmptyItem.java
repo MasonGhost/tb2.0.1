802f53fea1252f05687d5fbf949ed8bc4cf365af
Jliuer <jliuer@aliyun.com>
2017-03-30 10:41:34 +0800

    (#105) code 资讯详情三个界面类型

--- app/src/main/java/com/zhiyicx/thinksnsplus/modules/information/adapter/InfoDetailCommentEmptyItem.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/information/adapter/InfoDetailCommentEmptyItem.java
@@ -1,5 +1,9 @@
 package com.zhiyicx.thinksnsplus.modules.information.adapter;
 
+import android.text.TextUtils;
+
+import com.zhiyicx.baseproject.widget.EmptyView;
+import com.zhiyicx.thinksnsplus.R;
 import com.zhiyicx.thinksnsplus.data.beans.InfoCommentListBean;
 import com.zhy.adapter.recyclerview.base.ItemViewDelegate;
 import com.zhy.adapter.recyclerview.base.ViewHolder;
@@ -13,17 +17,19 @@ import com.zhy.adapter.recyclerview.base.ViewHolder;
 public class InfoDetailCommentEmptyItem implements ItemViewDelegate<InfoCommentListBean> {
     @Override
     public int getItemViewLayoutId() {
-        return 0;
+        return R.layout.item_dynamic_detail_comment_empty;
     }
 
     @Override
     public boolean isForViewType(InfoCommentListBean item, int position) {
-        return false;
+        return TextUtils.isEmpty(item.getComment_content()) && position != 0;
     }
 
     @Override
     public void convert(ViewHolder holder, InfoCommentListBean infoCommentListBean,
                         InfoCommentListBean lastT, int position) {
-
+        EmptyView emptyView = holder.getView(R.id.comment_emptyview);
+        emptyView.setNeedTextTip(false);
+        emptyView.setErrorType(EmptyView.STATE_NODATA_ENABLE_CLICK);
     }
 }
--- app/src/main/java/com/zhiyicx/thinksnsplus/modules/information/adapter/InfoDetailCommentItem.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/information/adapter/InfoDetailCommentItem.java
@@ -1,6 +1,22 @@
 package com.zhiyicx.thinksnsplus.modules.information.adapter;
 
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.zhiyicx.baseproject.config.ImageZipConfig;
+import com.zhiyicx.baseproject.impl.imageloader.glide.GlideImageConfig;
+import com.zhiyicx.baseproject.impl.imageloader.glide.transformation.GlideCircleTransform;
+import com.zhiyicx.baseproject.utils.ImageUtils;
+import com.zhiyicx.common.utils.TimeUtils;
+import com.zhiyicx.thinksnsplus.R;
+import com.zhiyicx.thinksnsplus.base.AppApplication;
+import com.zhiyicx.thinksnsplus.data.beans.InfoCommentListBean;
 import com.zhy.adapter.recyclerview.base.ItemViewDelegate;
+import com.zhy.adapter.recyclerview.base.ViewHolder;
+
+import java.util.List;
 
 /**
  * @Author Jliuer
@@ -8,5 +24,72 @@ import com.zhy.adapter.recyclerview.base.ItemViewDelegate;
  * @Email Jliuer@aliyun.com
  * @Description
  */
-public class InfoDetailCommentItem implements ItemViewDelegate {
+public class InfoDetailCommentItem implements ItemViewDelegate<InfoCommentListBean> {
+
+    private OnCommentItemListener mOnCommentItemListener;
+
+    public InfoDetailCommentItem(OnCommentItemListener onCommentItemListener) {
+        mOnCommentItemListener = onCommentItemListener;
+    }
+
+    @Override
+    public int getItemViewLayoutId() {
+        return R.layout.item_dynamic_detail_comment;
+    }
+
+    @Override
+    public boolean isForViewType(InfoCommentListBean item, int position) {
+        return !TextUtils.isEmpty(item.getComment_content());
+    }
+
+    @Override
+    public void convert(final ViewHolder holder, InfoCommentListBean infoCommentListBean,
+                        InfoCommentListBean lastT, final int position) {
+        AppApplication.AppComponentHolder.getAppComponent()
+                .imageLoader()
+                .loadImage(holder.getConvertView().getContext(), GlideImageConfig.builder()
+                        .url(ImageUtils.imagePathConvert(infoCommentListBean
+                                .getFromUserInfoBean()
+                                .getAvatar(), ImageZipConfig.IMAGE_26_ZIP))
+                        .placeholder(R.drawable.shape_default_image_circle)
+                        .transformation(new GlideCircleTransform(holder.getConvertView()
+                                .getContext()))
+                        .errorPic(R.drawable.shape_default_image_circle)
+                        .imagerView((ImageView) holder.getView(R.id.iv_headpic))
+                        .build()
+                );
+        holder.setText(R.id.tv_name, infoCommentListBean.getFromUserInfoBean().getName());
+        holder.setText(R.id.tv_time, TimeUtils.getTimeFriendlyNormal(infoCommentListBean
+                .getCreated_at()));
+        holder.setText(R.id.tv_content, setShowText(infoCommentListBean, position));
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mOnCommentItemListener.onItemClick(v,holder,position);
+            }
+        });
+    }
+
+    protected String setShowText(InfoCommentListBean infoCommentListBean, int position) {
+        return handleName(infoCommentListBean);
+    }
+
+    private String handleName(InfoCommentListBean infoCommentListBean) {
+        String content = "";
+        if (infoCommentListBean.getReply_to_user_id() != 0) { // 当没有回复者时，就是回复评论
+            content += " 回复 " + infoCommentListBean.getToUserInfoBean().getName() + " " +
+                    infoCommentListBean.getComment_content();
+        } else {
+            content = infoCommentListBean.getComment_content();
+        }
+        return content;
+    }
+
+    public void setOnCommentItemListener(OnCommentItemListener onCommentItemListener) {
+        mOnCommentItemListener = onCommentItemListener;
+    }
+
+    public interface OnCommentItemListener {
+        void onItemClick(View view, RecyclerView.ViewHolder holder, int position);
+    }
 }
