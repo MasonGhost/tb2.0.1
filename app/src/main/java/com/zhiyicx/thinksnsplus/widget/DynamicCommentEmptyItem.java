a067016818060486788f8961e2e157d9981e55f4
Jungle68 <335891510@qq.com>
2017-04-04 17:53:04 +0800

    （ #228）code 修复 动态详情评论删除完了后缺省图没有出现

---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/widget/DynamicCommentEmptyItem.java
@@ -0,0 +1,18 @@
+package com.zhiyicx.thinksnsplus.widget;
+
+import com.zhiyicx.thinksnsplus.data.beans.DynamicCommentBean;
+
+/**
+ * @Describe
+ * @Author Jungle68
+ * @Date 2017/3/10
+ * @Contact master.jungle68@gmail.com
+ */
+
+public class DynamicCommentEmptyItem extends EmptyItem<DynamicCommentBean> {
+
+    @Override
+    public boolean isForViewType(DynamicCommentBean item, int position) {
+        return item.getComment_content() == null;
+    }
+}
