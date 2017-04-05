3a58c04846c9bb3572e6787a07d9f12b3b423704
TNTYM\Administrator <Jliuer@aliyun.com>
2017-04-05 18:19:16 +0800

    (#75) code 【音乐专辑】 音乐专辑实体数据修改

--- app/src/main/java/com/zhiyicx/thinksnsplus/data/beans/MusicCommentListBean.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/data/beans/MusicCommentListBean.java
@@ -6,8 +6,12 @@ import com.zhiyicx.baseproject.base.BaseListBean;
 
 import org.greenrobot.greendao.annotation.Entity;
 import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Keep;
+import org.greenrobot.greendao.annotation.ToOne;
 import org.greenrobot.greendao.annotation.Unique;
 import org.greenrobot.greendao.annotation.Generated;
+import org.greenrobot.greendao.DaoException;
+import org.greenrobot.greendao.annotation.NotNull;
 
 /**
  * @Author Jliuer
@@ -38,9 +42,17 @@ public class MusicCommentListBean extends BaseListBean {
     private String comment_content;
     private int user_id;
     private int reply_to_user_id;
+    @ToOne(joinProperty = "user_id")
+    private UserInfoBean fromUserInfoBean;
+    @ToOne(joinProperty = "reply_to_user_id")
+    private UserInfoBean toUserInfoBean;
     private int music_id;
     private int special_id;
+    @Unique
+    private Long comment_mark;
     private int state = SEND_SUCCESS;
+
+
 
     public int getId() {
         return id;
@@ -140,6 +152,77 @@ public class MusicCommentListBean extends BaseListBean {
         this.state = state;
     }
 
+    public Long getComment_mark() {
+        return this.comment_mark;
+    }
+
+    public void setComment_mark(Long comment_mark) {
+        this.comment_mark = comment_mark;
+    }
+
+    @Keep
+    public UserInfoBean getToUserInfoBean() {
+        return toUserInfoBean;
+    }
+
+    @Keep
+    public void setToUserInfoBean(UserInfoBean toUserInfoBean) {
+        this.toUserInfoBean = toUserInfoBean;
+    }
+
+    @Keep
+    public UserInfoBean getFromUserInfoBean() {
+        return fromUserInfoBean;
+    }
+
+    @Keep
+    public void setFromUserInfoBean(UserInfoBean fromUserInfoBean) {
+        this.fromUserInfoBean = fromUserInfoBean;
+    }
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#delete(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 128553479)
+    public void delete() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.delete(this);
+    }
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#refresh(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 1942392019)
+    public void refresh() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.refresh(this);
+    }
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#update(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 713229351)
+    public void update() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.update(this);
+    }
+
+    /** called by internal mechanisms, do not call yourself. */
+    @Generated(hash = 870552357)
+    public void __setDaoSession(DaoSession daoSession) {
+        this.daoSession = daoSession;
+        myDao = daoSession != null ? daoSession.getMusicCommentListBeanDao() : null;
+    }
+
     public MusicCommentListBean() {
     }
 
@@ -155,11 +238,10 @@ public class MusicCommentListBean extends BaseListBean {
         this.special_id = in.readInt();
     }
 
-    @Generated(hash = 1918542083)
+    @Generated(hash = 1940892176)
     public MusicCommentListBean(Long _id, int id, String created_at, String updated_at,
-                                String comment_content, int user_id, int reply_to_user_id, int
-                                            music_id, int special_id,
-                                int state) {
+            String comment_content, int user_id, int reply_to_user_id, int music_id, int special_id,
+            Long comment_mark, int state) {
         this._id = _id;
         this.id = id;
         this.created_at = created_at;
@@ -169,6 +251,7 @@ public class MusicCommentListBean extends BaseListBean {
         this.reply_to_user_id = reply_to_user_id;
         this.music_id = music_id;
         this.special_id = special_id;
+        this.comment_mark = comment_mark;
         this.state = state;
     }
 
@@ -184,4 +267,14 @@ public class MusicCommentListBean extends BaseListBean {
             return new MusicCommentListBean[size];
         }
     };
+    /** Used to resolve relations */
+    @Generated(hash = 2040040024)
+    private transient DaoSession daoSession;
+    /** Used for active entity operations. */
+    @Generated(hash = 195439560)
+    private transient MusicCommentListBeanDao myDao;
+    @Generated(hash = 386266430)
+    private transient Integer fromUserInfoBean__resolvedKey;
+    @Generated(hash = 1650243776)
+    private transient Integer toUserInfoBean__resolvedKey;
 }
