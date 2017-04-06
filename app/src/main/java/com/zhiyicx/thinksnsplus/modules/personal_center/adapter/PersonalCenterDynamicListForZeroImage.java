3214b2eebac392aaf4ad9740cd92a4af7dc9b777
LC\ZYCX <450127106@qq.com>
2017-04-06 19:07:28 +0800

    （#102）code 调整0张图的布局，使得分割线在时间下方；修改时间显示控件复用导致的显示bug；

--- app/src/main/java/com/zhiyicx/thinksnsplus/modules/personal_center/adapter/PersonalCenterDynamicListForZeroImage.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/personal_center/adapter/PersonalCenterDynamicListForZeroImage.java
@@ -21,6 +21,10 @@ public class PersonalCenterDynamicListForZeroImage extends PersonalCenterDynamic
         super(context);
     }
 
+    @Override
+    public int getItemViewLayoutId() {
+        return super.getItemViewLayoutId();
+    }
 
     @Override
     public boolean isForViewType(DynamicBean item, int position) {
@@ -30,16 +34,5 @@ public class PersonalCenterDynamicListForZeroImage extends PersonalCenterDynamic
     @Override
     public void convert(ViewHolder holder, DynamicBean dynamicBean, DynamicBean lastT, int position) {
         super.convert(holder, dynamicBean, lastT, position);
-        // 对于0张图的动态，可能因为内容太少，而高度不够，导致分割线在左侧时间上，修改分割线的布局依赖,
-        // 让分割线在时间的下方
-        View timeContainer = holder.getView(R.id.ll_time_container);
-        View contentView = holder.getView(R.id.tv_content);
-        View dividerLine = holder.getView(R.id.v_line);
-        LogUtils.i("zero_image" + "--timeContainer   " + timeContainer.getBottom() + "--contentView   " + contentView.getBottom());
-        // 时间底部大于内容的底部，应该让分割线在时间的下面
-        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) dividerLine.getLayoutParams();
-        if (timeContainer.getBottom() > contentView.getBottom()) {
-            layoutParams.addRule(RelativeLayout.BELOW, R.id.ll_time_container);
-        }
     }
 }
