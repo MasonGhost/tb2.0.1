4c5c0b85f0e58e28404258628898fd712952c6ff
Jungle68 <335891510@qq.com>
2017-04-10 15:34:17 +0800

    （#83） code   添加推送消息基础属性

---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/data/beans/JpushMessageBean.java
@@ -0,0 +1,16 @@
+package com.zhiyicx.thinksnsplus.data.beans;
+
+/**
+ * @Describe
+ * @Author Jungle68
+ * @Date 2017/4/10
+ * @Contact master.jungle68@gmail.com
+ */
+
+public class JpushMessageBean {
+    
+    private String message;
+    private String type;
+    private String action;
+
+}
