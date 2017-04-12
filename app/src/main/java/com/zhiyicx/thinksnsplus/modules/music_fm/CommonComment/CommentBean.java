6bd73fc72a4a0bc55545c3265b33838db2fbbdd9
TNTYM\Administrator <Jliuer@aliyun.com>
2017-04-12 15:05:14 +0800

    (#75) code 【评论】评论模块的简单抽取

---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/music_fm/CommonComment/CommentBean.java
@@ -0,0 +1,10 @@
+package com.zhiyicx.thinksnsplus.modules.music_fm.CommonComment;
+
+/**
+ * @Author Jliuer
+ * @Date 2017/04/12/10:08
+ * @Email Jliuer@aliyun.com
+ * @Description
+ */
+public class CommentBean {
+}
---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/music_fm/CommonComment/SendComment.java
@@ -0,0 +1,11 @@
+package com.zhiyicx.thinksnsplus.modules.music_fm.CommonComment;
+
+/**
+ * @Author Jliuer
+ * @Date 2017/04/12/9:24
+ * @Email Jliuer@aliyun.com
+ * @Description
+ */
+
+public class SendComment {
+}
