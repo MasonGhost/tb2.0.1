df95232779fab14000d2df895275957400aec36a
TNTYM\Administrator <Jliuer@aliyun.com>
2017-05-09 17:21:21 +0800

    (#359) code 音乐逻辑处理类优化

--- app/src/main/java/com/zhiyicx/thinksnsplus/data/source/repository/MusicRepository.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/data/source/repository/MusicRepository.java
@@ -1,29 +1,19 @@
 package com.zhiyicx.thinksnsplus.data.source.repository;
 
-import android.app.Application;
-import android.content.Context;
-
 import com.zhiyicx.baseproject.base.TSListFragment;
-import com.zhiyicx.baseproject.config.ApiConfig;
 import com.zhiyicx.common.base.BaseJson;
 import com.zhiyicx.thinksnsplus.base.AppApplication;
-import com.zhiyicx.thinksnsplus.config.BackgroundTaskRequestMethodConfig;
-import com.zhiyicx.thinksnsplus.data.beans.BackgroundRequestTaskBean;
 import com.zhiyicx.thinksnsplus.data.beans.MusicAlbumListBean;
 import com.zhiyicx.thinksnsplus.data.source.local.MusicAlbumListBeanGreenDaoImpl;
 import com.zhiyicx.thinksnsplus.data.source.remote.MusicClient;
 import com.zhiyicx.thinksnsplus.data.source.remote.ServiceManager;
 import com.zhiyicx.thinksnsplus.modules.music_fm.music_album_list.MusicContract;
-import com.zhiyicx.thinksnsplus.service.backgroundtask.BackgroundTaskManager;
 
-import java.util.HashMap;
 import java.util.List;
 
 import javax.inject.Inject;
 
 import rx.Observable;
-import rx.functions.Action1;
-import rx.schedulers.Schedulers;
 
 /**
  * @Author Jliuer
