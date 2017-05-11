f3368143c1439d5fa3e48c49952de3e36ccfc77b
Jungle68 <335891510@qq.com>
2017-05-11 17:00:38 +0800

    (#365)   code  update flushmessage's  max_id for local data

--- app/src/main/java/com/zhiyicx/thinksnsplus/modules/home/message/MessagePresenter.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/home/message/MessagePresenter.java
@@ -10,6 +10,7 @@ import com.zhiyicx.common.mvp.BasePresenter;
 import com.zhiyicx.common.utils.ActivityHandler;
 import com.zhiyicx.common.utils.SharePreferenceUtils;
 import com.zhiyicx.common.utils.TimeUtils;
+import com.zhiyicx.common.utils.log.LogUtils;
 import com.zhiyicx.imsdk.db.dao.ConversationDao;
 import com.zhiyicx.imsdk.db.dao.MessageDao;
 import com.zhiyicx.imsdk.entity.AuthData;
@@ -573,6 +574,8 @@ public class MessagePresenter extends BasePresenter<MessageContract.Repository, 
                 max_user_nums = MAX_USER_NUMS_COMMENT;
                 CommentedBean lastCommentedBean = mCommentedBeanGreenDao.getLastData();
                 if (lastCommentedBean != null && flushMessage.getMax_id() != 0 && lastCommentedBean.getId() > flushMessage.getMax_id()) {// 如果本地查看的数据 id 已经大于 新消息的 id 说明已经读取过了
+                    LogUtils.d(TAG,"---1---"+lastCommentedBean.toString());
+                    LogUtils.d(TAG,"---2--"+flushMessage.toString());
                     flushMessage.setCount(0);
                 }
                 break;
@@ -671,6 +674,7 @@ public class MessagePresenter extends BasePresenter<MessageContract.Repository, 
         }
         localFlushMessage.setCount(netFlushMessage.getCount() + localFlushMessage.getCount());
         localFlushMessage.setTime(netFlushMessage.getTime());
+        localFlushMessage.setMax_id(netFlushMessage.getMax_id());
         if (netFlushMessage.getCount() >= MAX_USER_NUMS_COMMENT) {
             localFlushMessage.setUids(netFlushMessage.getUids());
         } else {
