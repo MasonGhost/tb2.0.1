4e08acf2ea35662fe76ecc658152f18a074999e8
TNTYM\Administrator <Jliuer@aliyun.com>
2017-05-15 17:21:59 +0800

    (#394) code 屏幕中间弹窗

---
+++ baseproject/src/main/java/com/zhiyicx/baseproject/widget/popwindow/CenterPopWindow.java
@@ -0,0 +1,12 @@
+package com.zhiyicx.baseproject.widget.popwindow;
+
+import com.zhiyicx.common.widget.popwindow.CustomPopupWindow;
+
+/**
+ * @Author Jliuer
+ * @Date 2017/05/15/14:13
+ * @Email Jliuer@aliyun.com
+ * @Description
+ */
+public class CenterPopWindow extends CustomPopupWindow {
+}
