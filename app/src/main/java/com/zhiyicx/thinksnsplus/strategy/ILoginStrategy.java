2357f9cd7c3e7b7037850496dc20aed207235e92
TNTYM\Administrator <Jliuer@aliyun.com>
2017-05-15 11:00:27 +0800

    (#359) code 所有分享图片优化

---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/strategy/ILoginStrategy.java
@@ -0,0 +1,12 @@
+package com.zhiyicx.thinksnsplus.strategy;
+
+/**
+ * @Author Jliuer
+ * @Date 2017/05/15/9:31
+ * @Email Jliuer@aliyun.com
+ * @Description
+ */
+public interface ILoginStrategy {
+    // 是否为游客
+    boolean isTourist();
+}
