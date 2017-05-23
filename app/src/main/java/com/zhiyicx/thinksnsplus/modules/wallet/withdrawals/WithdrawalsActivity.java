a95e9464e074767458832d10913a0c36541b4983
TNTYM\Administrator <Jliuer@aliyun.com>
2017-05-23 10:53:39 +0800

    (#368) code 提现页面

---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/wallet/withdrawals/WithdrawalsActivity.java
@@ -0,0 +1,18 @@
+package com.zhiyicx.thinksnsplus.modules.wallet.withdrawals;
+
+import android.support.v4.app.Fragment;
+
+import com.zhiyicx.baseproject.base.TSActivity;
+
+public class WithdrawalsActivity extends TSActivity {
+
+    @Override
+    protected Fragment getFragment() {
+        return null;
+    }
+
+    @Override
+    protected void componentInject() {
+
+    }
+}
---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/wallet/withdrawals/WithdrawalsFragment.java
@@ -0,0 +1,35 @@
+package com.zhiyicx.thinksnsplus.modules.wallet.withdrawals;
+
+import android.view.View;
+
+import com.zhiyicx.baseproject.base.TSFragment;
+import com.zhiyicx.thinksnsplus.R;
+
+/**
+ * @Author Jliuer
+ * @Date 2017/05/23/10:24
+ * @Email Jliuer@aliyun.com
+ * @Description
+ */
+public class WithdrawalsFragment extends TSFragment {
+
+    @Override
+    public void setPresenter(Object presenter) {
+
+    }
+
+    @Override
+    protected void initView(View rootView) {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected int getBodyLayoutId() {
+        return R.layout.fragment_withdrawals;
+    }
+}
