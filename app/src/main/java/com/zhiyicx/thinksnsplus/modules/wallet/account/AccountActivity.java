ac36da1f8629bb99c535bb079102cd8dbcaefd4a
TNTYM\Administrator <Jliuer@aliyun.com>
2017-05-23 16:07:40 +0800

    (#368) code 账户详情界面

---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/wallet/account/AccountActivity.java
@@ -0,0 +1,12 @@
+package com.zhiyicx.thinksnsplus.modules.wallet.account;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AccountActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/wallet/account/AccountFragment.java
@@ -0,0 +1,12 @@
+package com.zhiyicx.thinksnsplus.modules.wallet.account;
+
+import com.zhiyicx.baseproject.base.TSFragment;
+
+/**
+ * @Author Jliuer
+ * @Date 2017/05/23/15:02
+ * @Email Jliuer@aliyun.com
+ * @Description
+ */
+public class AccountFragment extends TSFragment {
+}
