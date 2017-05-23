a301d742da6c6246727e38a06acde5c56d1bbdcc
TNTYM\Administrator <Jliuer@aliyun.com>
2017-05-23 11:16:16 +0800

    (#376) code 提现明细界面

---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/wallet/withdrawals/detail/WithdrawalsDetailFragment.java
@@ -0,0 +1,24 @@
+package com.zhiyicx.thinksnsplus.modules.wallet.withdrawals.detail;
+
+import android.support.v7.widget.RecyclerView;
+
+import com.zhiyicx.baseproject.base.TSListFragment;
+
+/**
+ * @Author Jliuer
+ * @Date 2017/05/23/10:56
+ * @Email Jliuer@aliyun.com
+ * @Description
+ */
+public class WithdrawalsDetailFragment extends TSListFragment {
+
+    @Override
+    public void setPresenter(Object presenter) {
+
+    }
+
+    @Override
+    protected RecyclerView.Adapter getAdapter() {
+        return null;
+    }
+}
