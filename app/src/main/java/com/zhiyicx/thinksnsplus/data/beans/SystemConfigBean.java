568eb87cc24dbace84e3c2a610bf317f08a4908d
Jungle68 <335891510@qq.com>
2017-05-27 11:37:21 +0800

    ( #407)  code  add balance ratio

--- app/src/main/java/com/zhiyicx/thinksnsplus/data/beans/SystemConfigBean.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/data/beans/SystemConfigBean.java
@@ -15,13 +15,24 @@ import java.util.List;
 public class SystemConfigBean implements Serializable {
 
     /**
+     * wallet:ratio: 200 // 转换显示余额的比例，百分比。（200 就表示 200%）
      * im_serve : 127.0.0.1:9900
      * im_helper : [{"uid":"1","url":"https://plus.io/users/1"}]
      */
+    @SerializedName("wallet:ratio")
+    private int wallet_ratio;
     @SerializedName("im:serve")
     private String im_serve;
     @SerializedName("im:helper")
     private List<ImHelperBean> im_helper;
+
+    public int getWallet_ratio() {
+        return wallet_ratio;
+    }
+
+    public void setWallet_ratio(int wallet_ratio) {
+        this.wallet_ratio = wallet_ratio;
+    }
 
     public String getIm_serve() {
         return im_serve;
@@ -86,7 +97,8 @@ public class SystemConfigBean implements Serializable {
     @Override
     public String toString() {
         return "SystemConfigBean{" +
-                "im_serve='" + im_serve + '\'' +
+                "wallet_ratio=" + wallet_ratio +
+                ", im_serve='" + im_serve + '\'' +
                 ", im_helper=" + im_helper +
                 '}';
     }
