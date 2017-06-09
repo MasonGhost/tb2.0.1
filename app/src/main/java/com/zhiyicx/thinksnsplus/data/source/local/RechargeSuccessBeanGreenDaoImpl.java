f6054d377aa5a0feb4b589be2c7e22e3f388e037
TNTYM\Administrator <Jliuer@aliyun.com>
2017-06-09 11:17:00 +0800

    (#428) code 钱包明细数据更新工具

---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/data/source/local/RechargeSuccessBeanGreenDaoImpl.java
@@ -0,0 +1,10 @@
+package com.zhiyicx.thinksnsplus.data.source.local;
+
+/**
+ * @Author Jliuer
+ * @Date 2017/06/09/9:49
+ * @Email Jliuer@aliyun.com
+ * @Description
+ */
+public class RechargeSuccessBeanGreenDaoImpl {
+}
---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/utils/recyclerview_diff/RechargeSuccessBeanDiff.java
@@ -0,0 +1,10 @@
+package com.zhiyicx.thinksnsplus.utils.recyclerview_diff;
+
+/**
+ * @Author Jliuer
+ * @Date 2017/06/09/10:11
+ * @Email Jliuer@aliyun.com
+ * @Description
+ */
+public class RechargeSuccessBeanDiff {
+}
