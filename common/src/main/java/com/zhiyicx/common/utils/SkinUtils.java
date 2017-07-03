9b2b7a91b36b24a34a0830ade0520f8541f95f0f
Jungle68 <335891510@qq.com>
2017-07-03 10:46:00 +0800

    (#452)   code  add  skin utils  to compat  setTextColor

---
+++ common/src/main/java/com/zhiyicx/common/utils/SkinUtils.java
@@ -0,0 +1,48 @@
+package com.zhiyicx.common.utils;
+
+import android.content.res.ColorStateList;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.ColorRes;
+import android.widget.TextView;
+
+import skin.support.content.res.SkinCompatResources;
+
+/**
+ * @Describe
+ * @Author Jungle68
+ * @Date 2017/6/28
+ * @Contact master.jungle68@gmail.com
+ */
+
+public class SkinUtils {
+
+
+    public static void setTextColor(TextView textView, @ColorRes int resId) {
+        textView.setTextColor(getColor(resId));
+    }
+
+    public static void setTextAppearance(TextView textView, int resId) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            textView.setTextAppearance(resId);
+        } else {
+            textView.setTextAppearance(textView.getContext(), resId);
+        }
+    }
+
+    public static int getColor(int resId) {
+        return SkinCompatResources.getInstance().getColor(resId);
+    }
+
+    public Drawable getDrawable(int resId) {
+        return SkinCompatResources.getInstance().getDrawable(resId);
+    }
+
+    public Drawable getMipmap(int resId) {
+        return SkinCompatResources.getInstance().getDrawable(resId);
+    }
+
+    public ColorStateList getColorStateList(int resId) {
+        return SkinCompatResources.getInstance().getColorStateList(resId);
+    }
+}
