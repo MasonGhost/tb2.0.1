617a4339c38a10c9afd171ec5a7302813e2d4c1c
狂奔的唐玄奘 <1310683823@qq.com>
2017-07-14 18:29:25 +0800

    (#409) code 钱包充值提现规则

---
+++ common/src/main/java/com/zhiyicx/common/utils/ClickableMovementMethod.java
@@ -0,0 +1,12 @@
+package com.zhiyicx.thinksnsplus.modules.dynamic.list.adapter;
+
+import android.text.method.BaseMovementMethod;
+
+/**
+ * @Author Jliuer
+ * @Date 2017/07/14/16:49
+ * @Email Jliuer@aliyun.com
+ * @Description
+ */
+public class ClickableMovementMethod extends BaseMovementMethod {
+}
