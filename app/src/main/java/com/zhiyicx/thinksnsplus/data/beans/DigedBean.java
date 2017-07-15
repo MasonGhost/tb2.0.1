4f7c26a9c20b01adf725c36a092f38f04afd2bbe
Jungle68 <335891510@qq.com>
2017-07-15 16:08:22 +0800

    build

--- app/src/main/java/com/zhiyicx/thinksnsplus/data/beans/DigedBean.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/data/beans/DigedBean.java
@@ -333,9 +333,7 @@ public class DigedBean extends BaseListBean {
         myDao.update(this);
     }
 
-    /**
-     * called by internal mechanisms, do not call yourself.
-     */
+    /** called by internal mechanisms, do not call yourself. */
     @Generated(hash = 1633631379)
     public void __setDaoSession(DaoSession daoSession) {
         this.daoSession = daoSession;
