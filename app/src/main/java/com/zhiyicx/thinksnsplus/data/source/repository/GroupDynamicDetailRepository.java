43c09ac63a647c129c0bcd3a9c00eb068def154f
CatherineFXX <648129313@qq.com>
2017-07-19 10:37:20 +0800

    (#530) 添加圈子动态详情页面

---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/data/source/repository/GroupDynamicDetailRepository.java
@@ -0,0 +1,32 @@
+package com.zhiyicx.thinksnsplus.data.source.repository;
+
+import com.zhiyicx.common.base.BaseJson;
+import com.zhiyicx.thinksnsplus.data.beans.FollowFansBean;
+import com.zhiyicx.thinksnsplus.data.source.remote.ServiceManager;
+import com.zhiyicx.thinksnsplus.modules.channel.group_dynamic.GroupDynamicDetailContract;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import rx.Observable;
+
+/**
+ * @author Catherine
+ * @describe
+ * @date 2017/7/18
+ * @contact email:648129313@qq.com
+ */
+
+public class GroupDynamicDetailRepository extends BaseDynamicRepository implements GroupDynamicDetailContract.Repository{
+
+    @Inject
+    public GroupDynamicDetailRepository(ServiceManager serviceManager) {
+        super(serviceManager);
+    }
+
+    @Override
+    public Observable<BaseJson<List<FollowFansBean>>> getUserFollowState(String user_ids) {
+        return null;
+    }
+}
