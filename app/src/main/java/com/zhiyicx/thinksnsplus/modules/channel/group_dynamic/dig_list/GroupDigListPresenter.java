0774c49a27580c41d773c2b3b0adac693bca50a4
CatherineFXX <648129313@qq.com>
2017-07-20 12:03:57 +0800

    (#530) 点赞列表关注与列表接口处理

--- app/src/main/java/com/zhiyicx/thinksnsplus/modules/channel/group_dynamic/dig_list/GroupDigListPresenter.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/channel/group_dynamic/dig_list/GroupDigListPresenter.java
@@ -1,15 +1,26 @@
 package com.zhiyicx.thinksnsplus.modules.channel.group_dynamic.dig_list;
 
+import com.zhiyicx.baseproject.config.ApiConfig;
 import com.zhiyicx.common.dagger.scope.FragmentScoped;
+import com.zhiyicx.common.utils.log.LogUtils;
 import com.zhiyicx.thinksnsplus.base.AppBasePresenter;
+import com.zhiyicx.thinksnsplus.base.BaseSubscribeForV2;
+import com.zhiyicx.thinksnsplus.config.BackgroundTaskRequestMethodConfig;
+import com.zhiyicx.thinksnsplus.data.beans.BackgroundRequestTaskBean;
 import com.zhiyicx.thinksnsplus.data.beans.FollowFansBean;
 import com.zhiyicx.thinksnsplus.data.beans.GroupDynamicLikeListBean;
+import com.zhiyicx.thinksnsplus.data.source.local.FollowFansBeanGreenDaoImpl;
+import com.zhiyicx.thinksnsplus.service.backgroundtask.BackgroundTaskManager;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
 import java.util.List;
 
 import javax.inject.Inject;
+
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * @author Catherine
@@ -22,13 +33,35 @@ public class GroupDigListPresenter extends AppBasePresenter<GroupDigListContract
         implements GroupDigListContract.Presenter{
 
     @Inject
+    FollowFansBeanGreenDaoImpl mFollowFansBeanGreenDao;
+
+    @Inject
     public GroupDigListPresenter(GroupDigListContract.Repository repository, GroupDigListContract.View rootView) {
         super(repository, rootView);
     }
 
     @Override
     public void requestNetData(Long maxId, boolean isLoadMore) {
+        mRepository.getGroupDynamicDigList(mRootView.getDynamicBean().getGroup_id(), mRootView.getDynamicBean().getId(), maxId)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new BaseSubscribeForV2<List<FollowFansBean>>() {
+                    @Override
+                    protected void onSuccess(List<FollowFansBean> data) {
+                        LogUtils.i("digList_netData" + data.toString());
+                        mRootView.onNetResponseSuccess(data, isLoadMore);
+                    }
 
+                    @Override
+                    protected void onFailure(String message, int code) {
+                        mRootView.showMessage(message);
+                    }
+
+                    @Override
+                    protected void onException(Throwable throwable) {
+                        mRootView.onResponseError(throwable, isLoadMore);
+                    }
+                });
     }
 
     @Override
@@ -43,7 +76,30 @@ public class GroupDigListPresenter extends AppBasePresenter<GroupDigListContract
 
     @Override
     public void handleFollowUser(int position, FollowFansBean followFansBean) {
-
+        BackgroundRequestTaskBean backgroundRequestTaskBean = null;
+        if (followFansBean.getOrigin_follow_status() == FollowFansBean.UNFOLLOWED_STATE) {
+            // 当前未关注，进行关注
+            followFansBean.setOrigin_follow_status(FollowFansBean.IFOLLOWED_STATE);
+            // 进行后台任务请求
+            backgroundRequestTaskBean = new BackgroundRequestTaskBean();
+            backgroundRequestTaskBean.setMethodType(BackgroundTaskRequestMethodConfig.POST);
+            backgroundRequestTaskBean.setPath(ApiConfig.APP_PATH_FOLLOW_USER);
+        } else {
+            // 已关注，取消关注
+            followFansBean.setOrigin_follow_status(FollowFansBean.UNFOLLOWED_STATE);
+            // 进行后台任务请求
+            backgroundRequestTaskBean = new BackgroundRequestTaskBean();
+            backgroundRequestTaskBean.setMethodType(BackgroundTaskRequestMethodConfig.DELETE);
+            backgroundRequestTaskBean.setPath(ApiConfig.APP_PATH_CANCEL_FOLLOW_USER);
+        }
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("user_id", followFansBean.getTargetUserId() + "");
+        backgroundRequestTaskBean.setParams(hashMap);
+        BackgroundTaskManager.getInstance(mContext).addBackgroundRequestTask
+                (backgroundRequestTaskBean);
+        // 本地数据库和ui进行刷新
+        mFollowFansBeanGreenDao.insertOrReplace(followFansBean);
+        mRootView.upDataFollowState(position);
     }
 
     @Override
