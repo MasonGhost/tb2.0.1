391eb0613cffd43a68e79bc56d5c1dac50b07b8a
CatherineFXX <648129313@qq.com>
2017-07-25 18:27:38 +0800

    (#562) 添加搜索专家页面

---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/data/source/repository/ExpertSearchRepository.java
@@ -0,0 +1,21 @@
+package com.zhiyicx.thinksnsplus.data.source.repository;
+
+import com.zhiyicx.thinksnsplus.data.source.remote.ServiceManager;
+import com.zhiyicx.thinksnsplus.modules.q_a.reward.expert_search.ExpertSearchContract;
+
+import javax.inject.Inject;
+
+/**
+ * @author Catherine
+ * @describe
+ * @date 2017/7/25
+ * @contact email:648129313@qq.com
+ */
+
+public class ExpertSearchRepository extends BasePublishQuestionRepository implements ExpertSearchContract.Repository{
+
+    @Inject
+    public ExpertSearchRepository(ServiceManager manager) {
+        super(manager);
+    }
+}
