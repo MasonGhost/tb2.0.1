d9cf3b1965b83f22ea9e81afed9d1a0aca318845
CatherineFXX <648129313@qq.com>
2017-07-25 14:32:42 +0800

    (#559) 添加悬赏页面

---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/q_a/reward/QA$RewardPresenterModule.java
@@ -0,0 +1,32 @@
+package com.zhiyicx.thinksnsplus.modules.q_a.reward;
+
+import com.zhiyicx.thinksnsplus.data.source.repository.QA$RewardRepository;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * @author Catherine
+ * @describe
+ * @date 2017/7/25
+ * @contact email:648129313@qq.com
+ */
+@Module
+public class QA$RewardPresenterModule {
+
+    private QA$RewardContract.View mView;
+
+    public QA$RewardPresenterModule(QA$RewardContract.View mView) {
+        this.mView = mView;
+    }
+
+    @Provides
+    public QA$RewardContract.View provideQA$RewardContractView(){
+        return mView;
+    }
+
+    @Provides
+    public QA$RewardContract.Repository provideQA$RewardContractRepository(QA$RewardRepository rewardRepository){
+        return rewardRepository;
+    }
+}
