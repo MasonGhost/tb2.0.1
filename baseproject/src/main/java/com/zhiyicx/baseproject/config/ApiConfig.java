3d8552efbe870224ff1b2a5ce1073f8214330d32
Jungle68 <335891510@qq.com>
2017-07-28 09:47:46 +0800

    build 0.8.21 beta

--- baseproject/src/main/java/com/zhiyicx/baseproject/config/ApiConfig.java
+++ baseproject/src/main/java/com/zhiyicx/baseproject/config/ApiConfig.java
@@ -32,11 +32,11 @@ public class ApiConfig {
     public static final boolean APP_IS_NEED_SSH_CERTIFICATE = false;// 在线测试服务器 2
 //    public static final String APP_DOMAIN = "https://plus.medz.cn/";// 在线测试服务器 2
 
-    public static final String APP_DOMAIN = "http://test-plus.zhibocloud.cn/";// 在线测试服务器
-    public static final String APP_IM_DOMAIN = "ws://test-plus.zhibocloud.cn:9900";// im 在线测试服务器
+//    public static final String APP_DOMAIN = "http://test-plus.zhibocloud.cn/";// 在线测试服务器
+//    public static final String APP_IM_DOMAIN = "ws://test-plus.zhibocloud.cn:9900";// im 在线测试服务器
 //
-//    public static final String APP_DOMAIN = "http://tsplus.zhibocloud.cn/";// 正式服务器
-//    public static final String APP_IM_DOMAIN = "ws://tsplus.zhibocloud.cn:9900";// im 正式服务器
+    public static final String APP_DOMAIN = "http://tsplus.zhibocloud.cn/";// 正式服务器
+    public static final String APP_IM_DOMAIN = "ws://tsplus.zhibocloud.cn:9900";// im 正式服务器
 
 
     public static final String URL_ABOUT_US = APP_DOMAIN + "api/" + API_VERSION + "/system/about";// 关于我们网站
