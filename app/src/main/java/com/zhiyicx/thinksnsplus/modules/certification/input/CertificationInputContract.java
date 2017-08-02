ff8519556a0c0580b21324b20f98bbfa07a2a060
CatherineFXX <648129313@qq.com>
2017-08-02 18:16:14 +0800

    (#591) 添加输入认证信息的页面

---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/certification/input/CertificationInputContract.java
@@ -0,0 +1,26 @@
+package com.zhiyicx.thinksnsplus.modules.certification.input;
+
+import com.zhiyicx.common.mvp.i.IBasePresenter;
+import com.zhiyicx.common.mvp.i.IBaseView;
+
+/**
+ * @author Catherine
+ * @describe
+ * @date 2017/8/2
+ * @contact email:648129313@qq.com
+ */
+
+public interface CertificationInputContract {
+
+    interface View extends IBaseView<Presenter>{
+
+    }
+
+    interface Presenter extends IBasePresenter{
+
+    }
+
+    interface Repository{
+
+    }
+}
