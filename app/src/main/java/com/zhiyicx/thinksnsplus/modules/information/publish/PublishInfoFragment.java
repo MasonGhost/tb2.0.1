64c499e0812ef1e3eeb95c1a14bf4bbf21fc12bf
狂奔的唐玄奘 <1310683823@qq.com>
2017-08-07 14:49:21 +0800

    (#613) code 发布资讯图文混排

--- app/src/main/java/com/zhiyicx/thinksnsplus/modules/information/publish/PublishInfoFragment.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/information/publish/PublishInfoFragment.java
@@ -1,10 +1,29 @@
 package com.zhiyicx.thinksnsplus.modules.information.publish;
 
+import android.content.Intent;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
+import com.jakewharton.rxbinding.view.RxView;
 import com.zhiyicx.baseproject.base.TSFragment;
+import com.zhiyicx.baseproject.impl.photoselector.DaggerPhotoSelectorImplComponent;
+import com.zhiyicx.baseproject.impl.photoselector.ImageBean;
+import com.zhiyicx.baseproject.impl.photoselector.PhotoSelectorImpl;
+import com.zhiyicx.baseproject.impl.photoselector.PhotoSeletorImplModule;
+import com.zhiyicx.baseproject.widget.popwindow.ActionPopupWindow;
+import com.zhiyicx.common.utils.SkinUtils;
+import com.zhiyicx.common.utils.log.LogUtils;
 import com.zhiyicx.thinksnsplus.R;
+import com.zhiyicx.thinksnsplus.modules.q_a.publish.detail.xrichtext.RichTextEditor;
+import com.zhiyicx.thinksnsplus.widget.UserInfoInroduceInputView;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * @Author Jliuer
@@ -12,7 +31,24 @@ import com.zhiyicx.thinksnsplus.R;
  * @Email Jliuer@aliyun.com
  * @Description
  */
-public class PublishInfoFragment extends TSFragment<PublishInfoContract.Presenter> implements PublishInfoContract.View {
+public class PublishInfoFragment extends TSFragment<PublishInfoContract.Presenter>
+        implements PublishInfoContract.View, PhotoSelectorImpl.IPhotoBackListener {
+
+    @BindView(R.id.et_info_title)
+    UserInfoInroduceInputView mEtInfoTitle;
+    @BindView(R.id.riche_test)
+    RichTextEditor mRicheTest;
+    @BindView(R.id.im_arrowc)
+    ImageView mImArrowc;
+    @BindView(R.id.im_pic)
+    ImageView mImPic;
+    @BindView(R.id.im_setting)
+    ImageView mImSetting;
+    @BindView(R.id.rl_publish_tool)
+    RelativeLayout mRlPublishTool;
+
+    private PhotoSelectorImpl mPhotoSelector;
+    private ActionPopupWindow mPhotoPopupWindow;// 图片选择弹框
 
     public static PublishInfoFragment getInstance(Bundle bundle) {
         PublishInfoFragment publishInfoFragment = new PublishInfoFragment();
@@ -21,17 +57,123 @@ public class PublishInfoFragment extends TSFragment<PublishInfoContract.Presente
     }
 
     @Override
-    protected void initView(View rootView) {
+    protected String setCenterTitle() {
+        return getString(R.string.edit_info);
+    }
 
+    @Override
+    protected String setLeftTitle() {
+        return getString(R.string.cancel);
+    }
+
+    @Override
+    protected String setRightTitle() {
+        return getString(R.string.next);
+    }
+
+    @Override
+    protected int setLeftImg() {
+        return 0;
+    }
+
+    @Override
+    protected void initView(View rootView) {
+        mToolbarLeft.setTextColor(SkinUtils.getColor(R.color.themeColor));
+        mToolbarRight.setTextColor(SkinUtils.getColor(R.color.themeColor));
+        initLisenter();
     }
 
     @Override
     protected void initData() {
-
+        mPhotoSelector = DaggerPhotoSelectorImplComponent
+                .builder()
+                .photoSeletorImplModule(new PhotoSeletorImplModule(this, this, PhotoSelectorImpl
+                        .NO_CRAFT))
+                .build().photoSelectorImpl();
     }
 
     @Override
     protected int getBodyLayoutId() {
         return R.layout.fragment_publish_info;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mPhotoSelector.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void getPhotoSuccess(List<ImageBean> photoList) {
+        if (photoList.isEmpty()) {
+            return;
+        }
+        String path = photoList.get(0).getImgUrl();
+        mRicheTest.insertImage(path, mRicheTest.getWidth());
+    }
+
+    @Override
+    public void getPhotoFailure(String errorMsg) {
+
+    }
+
+    /**
+     * 初始化图片选择弹框
+     */
+    private void initPhotoPopupWindow() {
+        if (mPhotoPopupWindow != null) {
+            mPhotoPopupWindow.show();
+            return;
+        }
+        mPhotoPopupWindow = ActionPopupWindow.builder()
+                .item1Str(getString(R.string.choose_from_photo))
+                .item2Str(getString(R.string.choose_from_camera))
+                .bottomStr(getString(R.string.cancel))
+                .isOutsideTouch(true)
+                .isFocus(true)
+                .backgroundAlpha(0.8f)
+                .with(getActivity())
+                .item1ClickListener(() -> {
+                    // 选择相册，单张
+                    mPhotoSelector.getPhotoListFromSelector(1, null);
+                    mPhotoPopupWindow.hide();
+                })
+                .item2ClickListener(() -> {
+                    // 选择相机，拍照
+                    mPhotoSelector.getPhotoFromCamera(null);
+                    mPhotoPopupWindow.hide();
+                })
+                .bottomClickListener(() -> mPhotoPopupWindow.hide()).build();
+        mPhotoPopupWindow.show();
+    }
+
+    @OnClick({R.id.im_arrowc, R.id.im_pic, R.id.im_setting})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.im_arrowc:
+                break;
+            case R.id.im_pic:
+                initPhotoPopupWindow();
+                break;
+            case R.id.im_setting:
+                break;
+        }
+    }
+
+    private void initLisenter() {
+        RxView.globalLayouts(mRlPublishTool).subscribe(aVoid -> {
+            Rect viewRect = new Rect();
+            mRlPublishTool.getGlobalVisibleRect(viewRect);
+            if (viewRect.top > mRlPublishTool.getHeight()) {
+                View rootview = getActivity().getWindow().getDecorView();
+                View aaa = rootview.findFocus();
+                if (aaa != null) {
+                    int textBottom = aaa.getBottom();
+                    LogUtils.d(textBottom);
+                    LogUtils.d(viewRect.toString());
+                    mRicheTest.smoothScrollBy(0, textBottom - viewRect.top);
+                }
+            }
+        });
     }
 }
