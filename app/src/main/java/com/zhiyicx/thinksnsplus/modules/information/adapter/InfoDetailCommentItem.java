5e9a12aa3e76a200108f46ee51d97671717ed9d8
CatherineFXX <648129313@qq.com>
2017-08-11 18:11:09 +0800

    (#619) 资讯评论bean添加置顶标志

--- app/src/main/java/com/zhiyicx/thinksnsplus/modules/information/adapter/InfoDetailCommentItem.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/information/adapter/InfoDetailCommentItem.java
@@ -4,6 +4,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.TextView;
 
 import com.jakewharton.rxbinding.view.RxView;
 import com.klinker.android.link_builder.Link;
@@ -73,6 +74,11 @@ public class InfoDetailCommentItem implements ItemViewDelegate<InfoCommentListBe
                 mOnCommentItemListener.onItemClick(v, holder, position);
             }
         });
+
+        TextView topFlag=holder.getView(R.id.tv_top_flag);
+        topFlag.setVisibility(!infoCommentListBean.isPinned() ? View.GONE : View.VISIBLE);
+        topFlag.setText(topFlag.getContext().getString(R.string.dynamic_top_flag));
+
         List<Link> links = setLiknks(holder, infoCommentListBean, position);
         if (!links.isEmpty()) {
             ConvertUtils.stringLinkConvert(holder.getView(R.id.tv_content), links);
@@ -87,12 +93,9 @@ public class InfoDetailCommentItem implements ItemViewDelegate<InfoCommentListBe
     }
 
     private void setUserInfoClick(View v, final UserInfoBean userInfoBean) {
-        RxView.clicks(v).subscribe(new Action1<Void>() {
-            @Override
-            public void call(Void aVoid) {
-                if (mOnCommentItemListener != null) {
-                    mOnCommentItemListener.onUserInfoClick(userInfoBean);
-                }
+        RxView.clicks(v).subscribe(aVoid -> {
+            if (mOnCommentItemListener != null) {
+                mOnCommentItemListener.onUserInfoClick(userInfoBean);
             }
         });
     }
@@ -109,21 +112,15 @@ public class InfoDetailCommentItem implements ItemViewDelegate<InfoCommentListBe
                     .setTextColorOfHighlightedLink(ContextCompat.getColor(holder.getConvertView().getContext(), R.color.general_for_hint)) // optional, defaults to holo blue
                     .setHighlightAlpha(.5f)                                     // optional, defaults to .15f
                     .setUnderlined(false)                                       // optional, defaults to true
-                    .setOnLongClickListener(new Link.OnLongClickListener() {
-                        @Override
-                        public void onLongClick(String clickedText) {
-                            if (mOnUserInfoLongClickListener != null) {
-                                mOnUserInfoLongClickListener.onUserInfoLongClick(infoCommentListBean.getToUserInfoBean());
-                            }
+                    .setOnLongClickListener(clickedText -> {
+                        if (mOnUserInfoLongClickListener != null) {
+                            mOnUserInfoLongClickListener.onUserInfoLongClick(infoCommentListBean.getToUserInfoBean());
                         }
                     })
-                    .setOnClickListener(new Link.OnClickListener() {
-                        @Override
-                        public void onClick(String clickedText) {
-                            // single clicked
-                            if (mOnUserInfoClickListener != null) {
-                                mOnUserInfoClickListener.onUserInfoClick(infoCommentListBean.getToUserInfoBean());
-                            }
+                    .setOnClickListener(clickedText -> {
+                        // single clicked
+                        if (mOnUserInfoClickListener != null) {
+                            mOnUserInfoClickListener.onUserInfoClick(infoCommentListBean.getToUserInfoBean());
                         }
                     });
             links.add(replyNameLink);
