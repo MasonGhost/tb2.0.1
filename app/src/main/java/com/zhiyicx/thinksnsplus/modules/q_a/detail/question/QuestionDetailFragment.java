005f1c106eed88715bf150f2d296f3473ecda3a7
CatherineFXX <648129313@qq.com>
2017-08-15 15:54:26 +0800

    (#674) 新增问题详情的页面

---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/q_a/detail/question/QuestionDetailFragment.java
@@ -0,0 +1,29 @@
+package com.zhiyicx.thinksnsplus.modules.q_a.detail.question;
+
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import com.zhiyicx.baseproject.base.TSListFragment;
+import com.zhiyicx.thinksnsplus.data.beans.qa.QAListInfoBean;
+
+/**
+ * @author Catherine
+ * @describe
+ * @date 2017/8/15
+ * @contact email:648129313@qq.com
+ */
+
+public class QuestionDetailFragment extends TSListFragment<QuestionDetailContract.Presenter, QAListInfoBean>
+        implements QuestionDetailContract.View{
+
+    public QuestionDetailFragment instance(Bundle bundle){
+        QuestionDetailFragment fragment = new QuestionDetailFragment();
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    protected RecyclerView.Adapter getAdapter() {
+        return null;
+    }
+}
