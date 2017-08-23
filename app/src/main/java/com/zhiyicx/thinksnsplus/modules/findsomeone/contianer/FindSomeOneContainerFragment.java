03d18fe67d6aab02957fef54930b78b75b96d07e
Jungle68 <335891510@qq.com>
2017-08-23 18:38:52 +0800

    ( #696)  code  fix this

--- app/src/main/java/com/zhiyicx/thinksnsplus/modules/findsomeone/contianer/FindSomeOneContainerFragment.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/findsomeone/contianer/FindSomeOneContainerFragment.java
@@ -52,6 +52,8 @@ public class FindSomeOneContainerFragment extends TSFragment<FindSomeOneContaine
     //声明定位回调监听器
     private AMapLocationClient mLocationClient;
 
+    private boolean mIscationed = false;
+
     private FindSomeOneContainerViewPagerFragment mFindSomeOneContainerViewPagerFragment;
 
     public static FindSomeOneContainerFragment newInstance(Bundle bundle) {
@@ -90,15 +92,22 @@ public class FindSomeOneContainerFragment extends TSFragment<FindSomeOneContaine
                 , R.id.fragment_container);
 
         initListener();
+
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
         mRxPermissions.request(android.Manifest.permission.ACCESS_COARSE_LOCATION)
                 .subscribe(aBoolean -> {
-                    if (aBoolean) {
+                    if (aBoolean && !mIscationed) {
                         initLocation();
                     } else {
                         mTvToolbarRight.setText(getString(R.string.emptyStr));
                     }
                 });
-
     }
 
     private void initLocation() {
@@ -126,6 +135,7 @@ public class FindSomeOneContainerFragment extends TSFragment<FindSomeOneContaine
         AMapLocationListener mAMapLocationListener = aMapLocation -> {
             if (aMapLocation != null) {
                 if (aMapLocation.getErrorCode() == 0) {
+                    mIscationed=true;
                     //可在其中解析amapLocation获取相应内容。
                     LogUtils.d("1 = " + aMapLocation.getAddress());
                     LogUtils.d("2 = " + aMapLocation.getCity());
@@ -156,7 +166,7 @@ public class FindSomeOneContainerFragment extends TSFragment<FindSomeOneContaine
     }
 
 
-    @OnClick({R.id.tv_toolbar_left,R.id.tv_toolbar_center, R.id.tv_toolbar_right_two, R.id.tv_toolbar_right})
+    @OnClick({R.id.tv_toolbar_left, R.id.tv_toolbar_center, R.id.tv_toolbar_right_two, R.id.tv_toolbar_right})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.tv_toolbar_left:
@@ -213,7 +223,7 @@ public class FindSomeOneContainerFragment extends TSFragment<FindSomeOneContaine
                         } else {
                             mTvToolbarRight.setText(result[result.length - 2]);
                         }
-                    }catch (Exception e1){
+                    } catch (Exception e1) {
 
                     }
                 }
