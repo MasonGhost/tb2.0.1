e431db11682c5937c1f6bbe3c5018516878a49cb
CatherineFXX <648129313@qq.com>
2017-09-20 14:46:31 +0800

    (#1073) 问题详情 相关支付的操作提示语修改

--- app/src/main/java/com/zhiyicx/thinksnsplus/modules/q_a/detail/question/QuestionDetailPresenter.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/q_a/detail/question/QuestionDetailPresenter.java
@@ -17,6 +17,7 @@ import com.zhiyicx.common.thridmanager.share.ShareContent;
 import com.zhiyicx.common.thridmanager.share.SharePolicy;
 import com.zhiyicx.common.utils.ConvertUtils;
 import com.zhiyicx.common.utils.RegexUtils;
+import com.zhiyicx.common.utils.log.LogUtils;
 import com.zhiyicx.thinksnsplus.R;
 import com.zhiyicx.thinksnsplus.base.AppBasePresenter;
 import com.zhiyicx.thinksnsplus.base.BaseSubscribeForV2;
@@ -234,21 +235,12 @@ public class QuestionDetailPresenter extends AppBasePresenter<QuestionDetailCont
     public void applyForExcellent(Long question_id) {
         Subscription subscription = handleWalletBlance((long) getSystemConfig().getExcellentQuestion())
                 .doOnSubscribe(() -> mRootView.showSnackLoadingMessage(mContext.getString(R
-                        .string.transaction_doing)))
-                .flatMap(new Func1<Object, Observable<BaseJsonV2<Object>>>() {
-                    @Override
-                    public Observable<BaseJsonV2<Object>> call(Object o) {
-                        return mRepository.applyForExcellent(question_id);
-                    }
-                })
-                .subscribeOn(Schedulers.io())
-                .doOnSubscribe(() -> mRootView.handleLoading(true, false, mContext.getString(R.string.bill_doing)))
-                .subscribeOn(AndroidSchedulers.mainThread())// subscribeOn & doOnSubscribe 的特殊性质
-                .observeOn(AndroidSchedulers.mainThread())
+                        .string.apply_doing)))
+                .flatMap(o -> mRepository.applyForExcellent(question_id))
                 .subscribe(new BaseSubscribeForV2<BaseJsonV2<Object>>() {
                     @Override
                     protected void onSuccess(BaseJsonV2<Object> data) {
-                        mRootView.handleLoading(false, true, data.getMessage().get(0));
+                        mRootView.handleLoading(false, true, mContext.getString(R.string.apply_for_success));
                     }
 
                     @Override
@@ -280,19 +272,14 @@ public class QuestionDetailPresenter extends AppBasePresenter<QuestionDetailCont
 
         Subscription subscription = handleWalletBlance((long) getSystemConfig().getOnlookQuestion())
                 .doOnSubscribe(() -> mRootView.showSnackLoadingMessage(mContext.getString(R
-                        .string.transaction_doing)))
-                .flatMap(new Func1<Object, Observable<BaseJsonV2<AnswerInfoBean>>>() {
-                    @Override
-                    public Observable<BaseJsonV2<AnswerInfoBean>> call(Object o) {
-                        return mRepository.payForOnlook(answer_id);
-                    }
-                })
+                        .string.pay_alert_ing)))
+                .flatMap(o -> mRepository.payForOnlook(answer_id))
                 .subscribe(new BaseSubscribeForV2<BaseJsonV2<AnswerInfoBean>>() {
                     @Override
                     protected void onSuccess(BaseJsonV2<AnswerInfoBean> data) {
                         mRootView.getListDatas().set(position, data.getData());
                         mRootView.refreshData(position);
-                        mRootView.showSnackMessage("成功", Prompt.DONE);
+                        mRootView.showSnackMessage(mContext.getString(R.string.pay_alert_success), Prompt.DONE);
                     }
 
                     @Override
@@ -304,7 +291,8 @@ public class QuestionDetailPresenter extends AppBasePresenter<QuestionDetailCont
                     @Override
                     protected void onException(Throwable throwable) {
                         super.onException(throwable);
-                        mRootView.showSnackErrorMessage(throwable.getMessage());
+                        LogUtils.d("Cathy", "payForOnlook // " + throwable.toString());
+                        mRootView.showSnackErrorMessage(mContext.getString(R.string.pay_alert_failed));
                     }
                 });
         addSubscrebe(subscription);
