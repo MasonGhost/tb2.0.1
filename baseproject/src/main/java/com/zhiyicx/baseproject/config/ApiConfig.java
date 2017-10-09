1021b2b5d135f3ad401b5da1f285cc371ab0db8f
Jungle68 <335891510@qq.com>
2017-10-09 10:12:13 +0800

    build

--- baseproject/src/main/java/com/zhiyicx/baseproject/config/ApiConfig.java
+++ baseproject/src/main/java/com/zhiyicx/baseproject/config/ApiConfig.java
@@ -34,8 +34,9 @@ public class ApiConfig {
     public static final String APP_DOMAIN_DEV = "http://dev.zhibocloud.cn/";// 模拟在线正式服务器
     public static final String APP_DOMAIN_TEST = "http://test-plus.zhibocloud.cn/";// 在线测试服务器
     public static final String APP_DOMAIN_FORMAL = "https://tsplus.zhibocloud.cn/";// 正式服务器
+    public static final String APP_DOMAIN_FOR_TEARCHER_QIAO = "http://192.168.2.200/";// 乔老师本地服务器
 
-    public static String APP_DOMAIN = APP_DOMAIN_FORMAL;
+    public static String APP_DOMAIN = APP_DOMAIN_FORMAL; // 默认使用正式服
 
     public static final String URL_ABOUT_US = "api/" + API_VERSION_2 + "/aboutus";// 关于我们网站
     public static final String URL_JIPU_SHOP = "http://demo.jipukeji.com";// 极铺购物地址
