46bb50e6dac989da0f253c461d74de5c356b8c04
狂奔的唐玄奘 <1310683823@qq.com>
2017-10-23 10:32:40 +0800

    (#1256) code add link-library

--- common/src/main/java/com/zhiyicx/common/utils/ConvertUtils.java
+++ common/src/main/java/com/zhiyicx/common/utils/ConvertUtils.java
@@ -256,8 +256,9 @@ public class ConvertUtils {
         List newList = new ArrayList();
         for (Iterator iter = list.iterator(); iter.hasNext(); ) {
             Object element = iter.next();
-            if (set.add(element))
+            if (set.add(element)) {
                 newList.add(element);
+            }
         }
         list.clear();
         list.addAll(newList);
@@ -272,9 +273,13 @@ public class ConvertUtils {
      * @return 16进制大写字符串
      */
     public static String bytes2HexString(byte[] bytes) {
-        if (bytes == null) return null;
+        if (bytes == null) {
+            return null;
+        }
         int len = bytes.length;
-        if (len <= 0) return null;
+        if (len <= 0) {
+            return null;
+        }
         char[] ret = new char[len << 1];
         for (int i = 0, j = 0; i < len; i++) {
             ret[j++] = hexDigits[bytes[i] >>> 4 & 0x0f];
@@ -292,7 +297,9 @@ public class ConvertUtils {
      * @return 字节数组
      */
     public static byte[] hexString2Bytes(String hexString) {
-        if (TextUtils.isEmpty(hexString)) return null;
+        if (TextUtils.isEmpty(hexString)) {
+            return null;
+        }
         int len = hexString.length();
         if (len % 2 != 0) {
             hexString = "0" + hexString;
@@ -329,7 +336,9 @@ public class ConvertUtils {
      * @return 字节数组
      */
     public static byte[] chars2Bytes(char[] chars) {
-        if (chars == null || chars.length <= 0) return null;
+        if (chars == null || chars.length <= 0) {
+            return null;
+        }
         int len = chars.length;
         byte[] bytes = new byte[len];
         for (int i = 0; i < len; i++) {
@@ -345,9 +354,13 @@ public class ConvertUtils {
      * @return 字符数组
      */
     public static char[] bytes2Chars(byte[] bytes) {
-        if (bytes == null) return null;
+        if (bytes == null) {
+            return null;
+        }
         int len = bytes.length;
-        if (len <= 0) return null;
+        if (len <= 0) {
+            return null;
+        }
         char[] chars = new char[len];
         for (int i = 0; i < len; i++) {
             chars[i] = (char) (bytes[i] & 0xff);
@@ -369,7 +382,9 @@ public class ConvertUtils {
      * @return 字节数
      */
     public static long memorySize2Byte(long memorySize, ConstantConfig.MemoryUnit unit) {
-        if (memorySize < 0) return -1;
+        if (memorySize < 0) {
+            return -1;
+        }
         switch (unit) {
             default:
             case BYTE:
@@ -397,7 +412,9 @@ public class ConvertUtils {
      * @return 以unit为单位的size
      */
     public static double byte2MemorySize(long byteNum, ConstantConfig.MemoryUnit unit) {
-        if (byteNum < 0) return -1;
+        if (byteNum < 0) {
+            return -1;
+        }
         switch (unit) {
             default:
             case BYTE:
@@ -531,7 +548,9 @@ public class ConvertUtils {
      */
     @SuppressLint("DefaultLocale")
     public static String millis2FitTimeSpan(long millis, int precision) {
-        if (millis <= 0 || precision <= 0) return null;
+        if (millis <= 0 || precision <= 0) {
+            return null;
+        }
         StringBuilder sb = new StringBuilder();
         String[] units = {"天", "小时", "分钟", "秒", "毫秒"};
         int[] unitLen = {86400000, 3600000, 60000, 1000, 1};
@@ -618,7 +637,9 @@ public class ConvertUtils {
      * @return outputStream子类
      */
     public static ByteArrayOutputStream input2OutputStream(InputStream is) {
-        if (is == null) return null;
+        if (is == null) {
+            return null;
+        }
         try {
             ByteArrayOutputStream os = new ByteArrayOutputStream();
             byte[] b = new byte[ConstantConfig.KB];
@@ -642,7 +663,9 @@ public class ConvertUtils {
      * @return inputStream子类
      */
     public ByteArrayInputStream output2InputStream(OutputStream out) {
-        if (out == null) return null;
+        if (out == null) {
+            return null;
+        }
         return new ByteArrayInputStream(((ByteArrayOutputStream) out).toByteArray());
     }
 
@@ -653,7 +676,9 @@ public class ConvertUtils {
      * @return 字节数组
      */
     public static byte[] inputStream2Bytes(InputStream is) {
-        if (is == null) return null;
+        if (is == null) {
+            return null;
+        }
         return input2OutputStream(is).toByteArray();
     }
 
@@ -664,7 +689,9 @@ public class ConvertUtils {
      * @return 输入流
      */
     public static InputStream bytes2InputStream(byte[] bytes) {
-        if (bytes == null || bytes.length <= 0) return null;
+        if (bytes == null || bytes.length <= 0) {
+            return null;
+        }
         return new ByteArrayInputStream(bytes);
     }
 
@@ -675,7 +702,9 @@ public class ConvertUtils {
      * @return 字节数组
      */
     public static byte[] outputStream2Bytes(OutputStream out) {
-        if (out == null) return null;
+        if (out == null) {
+            return null;
+        }
         return ((ByteArrayOutputStream) out).toByteArray();
     }
 
@@ -686,7 +715,9 @@ public class ConvertUtils {
      * @return 字节数组
      */
     public static OutputStream bytes2OutputStream(byte[] bytes) {
-        if (bytes == null || bytes.length <= 0) return null;
+        if (bytes == null || bytes.length <= 0) {
+            return null;
+        }
         ByteArrayOutputStream os = null;
         try {
             os = new ByteArrayOutputStream();
@@ -708,7 +739,9 @@ public class ConvertUtils {
      * @return 字符串
      */
     public static String inputStream2String(InputStream is, String charsetName) {
-        if (is == null || TextUtils.isEmpty(charsetName)) return null;
+        if (is == null || TextUtils.isEmpty(charsetName)) {
+            return null;
+        }
         try {
             return new String(inputStream2Bytes(is), charsetName);
         } catch (UnsupportedEncodingException e) {
@@ -725,7 +758,9 @@ public class ConvertUtils {
      * @return 输入流
      */
     public static InputStream string2InputStream(String string, String charsetName) {
-        if (string == null || TextUtils.isEmpty(charsetName)) return null;
+        if (string == null || TextUtils.isEmpty(charsetName)) {
+            return null;
+        }
         try {
             return new ByteArrayInputStream(string.getBytes(charsetName));
         } catch (UnsupportedEncodingException e) {
@@ -742,7 +777,9 @@ public class ConvertUtils {
      * @return 字符串
      */
     public static String outputStream2String(OutputStream out, String charsetName) {
-        if (out == null || TextUtils.isEmpty(charsetName)) return null;
+        if (out == null || TextUtils.isEmpty(charsetName)) {
+            return null;
+        }
         try {
             return new String(outputStream2Bytes(out), charsetName);
         } catch (UnsupportedEncodingException e) {
@@ -759,7 +796,9 @@ public class ConvertUtils {
      * @return 输入流
      */
     public static OutputStream string2OutputStream(String string, String charsetName) {
-        if (string == null || TextUtils.isEmpty(charsetName)) return null;
+        if (string == null || TextUtils.isEmpty(charsetName)) {
+            return null;
+        }
         try {
             return bytes2OutputStream(string.getBytes(charsetName));
         } catch (UnsupportedEncodingException e) {
@@ -776,7 +815,9 @@ public class ConvertUtils {
      * @return 字节数组
      */
     public static byte[] bitmap2Bytes(Bitmap bitmap, Bitmap.CompressFormat format) {
-        if (bitmap == null) return null;
+        if (bitmap == null) {
+            return null;
+        }
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         bitmap.compress(format, 100, baos);
         return baos.toByteArray();
@@ -892,7 +933,9 @@ public class ConvertUtils {
      * @return bitmap
      */
     public static Bitmap view2Bitmap(View view) {
-        if (view == null) return null;
+        if (view == null) {
+            return null;
+        }
         Bitmap ret = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(ret);
         Drawable bgDrawable = view.getBackground();
