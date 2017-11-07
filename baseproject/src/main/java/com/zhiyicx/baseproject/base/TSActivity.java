ce8f49688055bf8c3cc9f9b7f0839ab48f1758fb
Jungle68 <335891510@qq.com>
2017-11-07 10:45:17 +0800

    (#1324)   code  认证失败优化弹框内存泄漏

--- baseproject/src/main/java/com/zhiyicx/baseproject/base/TSActivity.java
+++ baseproject/src/main/java/com/zhiyicx/baseproject/base/TSActivity.java
@@ -1,12 +1,22 @@
 package com.zhiyicx.baseproject.base;
 
+import android.content.DialogInterface;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.view.KeyEvent;
 
 import com.umeng.analytics.MobclickAgent;
 import com.zhiyicx.baseproject.R;
 import com.zhiyicx.common.base.BaseActivity;
 import com.zhiyicx.common.mvp.BasePresenter;
+import com.zhiyicx.common.utils.ActivityHandler;
 import com.zhiyicx.common.utils.ActivityUtils;
+
+import rx.Observable;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action0;
+import rx.functions.Action1;
 
 /**
  * @Describe activity只是作为fragment的容器，具体的功能逻辑在fragment中完成
@@ -17,7 +27,17 @@ import com.zhiyicx.common.utils.ActivityUtils;
 
 public abstract class TSActivity<P extends BasePresenter, F extends Fragment> extends BaseActivity<P> {
 
+    /**
+     * 内容 fragment
+     */
     protected F mContanierFragment;
+
+    /**
+     * token 过期弹框，重新登录等
+     */
+    private AlertDialog mAlertdialog;
+    private Subscription mSubscribe;
+
 
     public F getContanierFragment() {
         return mContanierFragment;
@@ -55,5 +75,62 @@ public abstract class TSActivity<P extends BasePresenter, F extends Fragment> ex
     protected void onPause() {
         super.onPause();
         MobclickAgent.onPause(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mAlertdialog != null && mAlertdialog.isShowing()) {
+            mAlertdialog.dismiss();
+        }
+        if (mSubscribe != null && mSubscribe.isUnsubscribed()) {
+            mSubscribe.unsubscribe();
+        }
+    }
+
+    /**
+     * @param tipStr
+     * @param listener
+     */
+    public void showWarnningDialog(final String tipStr, final DialogInterface.OnClickListener listener) {
+        if (listener == null) {
+            return;
+        }
+        // 跳到登陆页面，销毁之前的所有页面,添加弹框处理提示
+        // 通过rxjava在主线程处理弹框
+        mSubscribe = Observable.empty()
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnCompleted(new Action0() {
+                    @Override
+                    public void call() {
+                        if ((mAlertdialog != null && mAlertdialog.isShowing())) {
+                            return;
+                        }
+                        mAlertdialog = new AlertDialog.Builder(TSActivity.this, R.style.TSWarningAlertDialogStyle)
+                                .setMessage(tipStr)
+                                .setOnKeyListener(new DialogInterface.OnKeyListener() {
+                                    @Override
+                                    public boolean onKey(DialogInterface dialog, int keyCode, KeyEvent event) {
+                                        return mAlertdialog.isShowing() && keyCode == KeyEvent.KEYCODE_BACK
+                                                && event.getRepeatCount() == 0;
+                                    }
+                                })
+                                .setPositiveButton(R.string.determine, listener)
+                                .create();
+                        mAlertdialog.setCanceledOnTouchOutside(false);
+                        try {
+                            mAlertdialog.show();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                })
+                .doOnError(new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable throwable) {
+                        throwable.printStackTrace();
+                    }
+                })
+                .subscribe();
     }
 }
