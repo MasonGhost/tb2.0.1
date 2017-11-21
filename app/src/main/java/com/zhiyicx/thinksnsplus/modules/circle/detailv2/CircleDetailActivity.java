be85d0e732ff5421c5af15a19a4c7e2eb3376483
狂奔的唐玄奘 <1310683823@qq.com>
2017-11-21 09:58:19 +0800

    (#1372) code 新的圈子详情界面搭建

---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/circle/detailv2/CircleDetailActivity.java
@@ -0,0 +1,20 @@
+package com.zhiyicx.thinksnsplus.modules.circle.detailv2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+import com.zhiyicx.baseproject.base.TSActivity;
+
+public class CircleDetailActivity extends TSActivity {
+
+    @Override
+    protected Fragment getFragment() {
+        return null;
+    }
+
+    @Override
+    protected void componentInject() {
+
+    }
+}
---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/circle/detailv2/CircleDetailFragment.java
@@ -0,0 +1,19 @@
+package com.zhiyicx.thinksnsplus.modules.circle.detailv2;
+
+import android.support.v7.widget.RecyclerView;
+
+import com.zhiyicx.baseproject.base.TSListFragment;
+
+/**
+ * @Author Jliuer
+ * @Date 2017/11/21/9:50
+ * @Email Jliuer@aliyun.com
+ * @Description
+ */
+public class CircleDetailFragment extends TSListFragment {
+
+    @Override
+    protected RecyclerView.Adapter getAdapter() {
+        return null;
+    }
+}
