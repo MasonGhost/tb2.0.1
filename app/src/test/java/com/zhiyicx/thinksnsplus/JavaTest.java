d084961b87151921884805d957ef2394ad3d6b44
Jungle68 <335891510@qq.com>
2017-11-24 11:46:54 +0800

    (#1349)  test  添加正则匹配测试

--- app/src/test/java/com/zhiyicx/thinksnsplus/JavaTest.java
+++ app/src/test/java/com/zhiyicx/thinksnsplus/JavaTest.java
@@ -112,7 +112,11 @@ public class JavaTest {
 
     @Test
     public void testInt() {
-        String test = "https://m.baidu.com/?from=1014254f#iact=wiseindex%2Ftabs%2Fnews%2Factivity%2Fnewsdetail%3D%257B%2522linkData%2522%253A%257B%2522name%2522%253A%2522iframe%252Fmib-iframe%2522%252C%2522id%2522%253A%2522feed%2522%252C%2522index%2522%253A0%252C%2522url%2522%253A%2522https%253A%252F%252Ffeed.baidu.com%252Ffeed%252Fdata%252Fwise%252Flandingpage%253Fs_type%253Dnews%2526dsp%253Dwise%2526nid%253D9385399617810917542%2526n_type%253D0%2526p_from%253D-1%2526innerIframe%253D1%2522%252C%2522title%2522%253Anull%257D%257D";
+        String test = "https://m.baidu.com/?from=1014254f#iact=wiseindex%2Ftabs%2Fnews%2Factivity%2Fnewsdetail%3D%257B%2522linkData%2522%253A%257B" +
+                "%2522name%2522%253A%2522iframe%252Fmib-iframe%2522%252C%2522id%2522%253A%2522feed%2522%252C%2522index%2522%253A0%252C%2522url%2522" +
+                "%253A%2522https%253A%252F%252Ffeed.baidu" +
+                ".com%252Ffeed%252Fdata%252Fwise%252Flandingpage%253Fs_type%253Dnews%2526dsp%253Dwise%2526nid%253D9385399617810917542%2526n_type" +
+                "%253D0%2526p_from%253D-1%2526innerIframe%253D1%2522%252C%2522title%2522%253Anull%257D%257D";
         System.out.println(test.matches(MarkdownConfig.NETSITE_FORMAT_));
 //        Matcher matcher=
         System.out.println(test.replaceAll(MarkdownConfig.NETSITE_FORMAT, MarkdownConfig.LINK_EMOJI + Link.DEFAULT_NET_SITE));
@@ -1501,5 +1505,32 @@ public class JavaTest {
         System.out.println("res = " + res);
         Assert.assertTrue(res < 0);
     }
+
+    /**
+     * 正则替换 markdown <>
+     */
+    @Test
+    public void testRex() {
+        // 内容
+        String content = "aaa<<httpbbb>>ccc<<ddd>>eee<<http:feiowjigrjw>>";
+        // 正则
+        Pattern p = Pattern.compile("\\<\\<(http.*?)\\>\\>");
+        Matcher m = p.matcher(content);
+
+        // 返回结果
+        StringBuffer result = new StringBuffer();
+        while (m.find()) {
+            System.out.println("正则匹配到的内容：" + m.group());
+            System.out.println("花括号里面的内容：" + m.group(1));
+            // 替换为 -
+            m.appendReplacement(result, m.group(1));
+        }
+        // 替换之前匹配到的内容
+        m.appendTail(result);
+
+        System.out.println("\n原始文本：" + content);
+        System.out.println("替换后的文本：" + result.toString());
+    }
+
 
 }
