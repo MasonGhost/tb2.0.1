ec41d19cf8538aa3c36352f7d5972557c3b57254
Jungle68 <335891510@qq.com>
2017-12-05 11:35:42 +0800

    (#1324)   code  直播赠送榜兼容服务器返回头像信息为空

--- zhibolibrary/src/main/java/com/zhiyicx/zhibolibrary/ui/components/RankHeadView.java
+++ zhibolibrary/src/main/java/com/zhiyicx/zhibolibrary/ui/components/RankHeadView.java
@@ -19,8 +19,9 @@ import com.zhy.autolayout.AutoRelativeLayout;
  * email:335891510@qq.com
  */
 public class RankHeadView extends AutoRelativeLayout {
-    private ImageView ivHeadpic,ivVertify;
+    private ImageView ivHeadpic, ivVertify;
     private Context mContext;
+
     public RankHeadView(Context context) {
         super(context);
         init(context);
@@ -38,15 +39,19 @@ public class RankHeadView extends AutoRelativeLayout {
     }
 
     private void init(Context context) {
-        mContext=context;
+        mContext = context;
         View newslayout = LayoutInflater.from(context).inflate(R.layout.zb_recycle_item_rank_headpic, this, true);
         ivHeadpic = (ImageView) newslayout.findViewById(R.id.iv_rank_gold_item_icon);
         ivVertify = (ImageView) newslayout.findViewById(R.id.iv_rank_gold_item_verified);
     }
-    public void setData(SearchResult data){
 
-        UiUtils.glideDisplayWithTrasform(data.user.avatar.getOrigin(), ivHeadpic, new GlideCircleTrasform(mContext));
+    public void setData(SearchResult data) {
+        if (data.user.avatar != null && data.user.avatar.getOrigin() != null) {
+            UiUtils.glideDisplayWithTrasform(data.user.avatar.getOrigin(), ivHeadpic, new GlideCircleTrasform(mContext));
+        } else {
+            UiUtils.glideDisplayWithTrasform("", ivHeadpic, new GlideCircleTrasform(mContext));
 
+        }
         ivVertify.setVisibility(data.user.is_verified == 1 ? View.VISIBLE : View.GONE);
 
 
