745eff1fc1a7766d86e931e610d6354923d67d65
CatherineFXX <648129313@qq.com>
2017-12-25 14:07:55 +0800

    feat: (#1579)  添加请求好友网络数据的方法

--- app/src/main/java/com/zhiyicx/thinksnsplus/data/source/remote/FollowFansClient.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/data/source/remote/FollowFansClient.java
@@ -14,6 +14,8 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 import rx.Observable;
 
+import static com.zhiyicx.baseproject.config.ApiConfig.APP_PATH_DELETE_USER_FRIENDS_LIST;
+
 /**
  * @author LiuChao
  * @describe
@@ -24,29 +26,18 @@ import rx.Observable;
 public interface FollowFansClient {
     /**
      * 获取用户粉丝列表
-     *
-     * @param user_id
-     * @param max_id
-     * @return
      */
     @GET(ApiConfig.APP_PATH_FANS_LIST)
     Observable<List<UserInfoBean>> getUserFansList(@Path("user_id") long user_id, @Query("offset") long max_id, @Query("limit") Integer limitCount);
 
     /**
      * 获取用户关注列表
-     *
-     * @param user_id
-     * @param max_id
-     * @return
      */
     @GET(ApiConfig.APP_PATH_FOLLOW_LIST)
     Observable<List<UserInfoBean>> getUserFollowsList(@Path("user_id") long user_id, @Query("offset") long max_id, @Query("limit") Integer limitCount);
 
     /**
      * 关注用户操作
-     *
-     * @param user_id
-     * @return
      */
     @FormUrlEncoded
     @PUT(ApiConfig.APP_PATH_FOLLOW_USER)
@@ -54,12 +45,19 @@ public interface FollowFansClient {
 
     /**
      * 取消用户关注
-     *
-     * @param user_id
-     * @return
      */
     @FormUrlEncoded
     @DELETE(ApiConfig.APP_PATH_CANCEL_FOLLOW_USER)
     Observable<Object> cancelFollowUser(@Field("user_id") long user_id);
+
+    /**
+     * 获取用户的好友列表
+     *
+     * @param max_id     offset
+     * @param limitCount 每页数据
+     * @return Observable
+     */
+    @GET(APP_PATH_DELETE_USER_FRIENDS_LIST)
+    Observable<List<UserInfoBean>> getUserFriendsList(@Query("offset") long max_id, @Query("limit") Integer limitCount);
 
 }
