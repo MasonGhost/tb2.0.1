5a4faedd2218b72e9db12a09a440262ff529fc5c
CatherineFXX <648129313@qq.com>
2017-12-29 14:40:14 +0800

    feat: (#1579) 根据设计图调整完善好友列表页面
    由于还未给出切图，所有icon不正确，给出后替换

--- app/src/main/java/com/zhiyicx/thinksnsplus/modules/home/mine/friends/MyFriendsListFragment.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/home/mine/friends/MyFriendsListFragment.java
@@ -1,16 +1,21 @@
 package com.zhiyicx.thinksnsplus.modules.home.mine.friends;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.zhiyicx.baseproject.base.TSListFragment;
 import com.zhiyicx.thinksnsplus.R;
 import com.zhiyicx.thinksnsplus.data.beans.UserInfoBean;
+import com.zhiyicx.thinksnsplus.modules.findsomeone.contianer.FindSomeOneContainerActivity;
 import com.zhiyicx.thinksnsplus.modules.home.mine.friends.search.SearchFriendsActivity;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+
+import butterknife.OnClick;
 
 /**
  * @author Catherine
@@ -28,27 +33,39 @@ public class MyFriendsListFragment extends TSListFragment<MyFriendsListContract.
     }
 
     @Override
-    protected boolean showToolBarDivider() {
-        return true;
+    protected int getBodyLayoutId() {
+        return R.layout.fragment_my_friends_list;
     }
 
     @Override
-    protected String setCenterTitle() {
-        return getString(R.string.chat_my_friends);
-    }
-
-    @Override
-    protected String setRightTitle() {
-        return "搜索";
-    }
-
-    @Override
-    protected void setRightClick() {
-        startActivity(new Intent(getActivity(), SearchFriendsActivity.class));
+    protected boolean showToolbar() {
+        return false;
     }
 
     @Override
     protected Long getMaxId(@NotNull List<UserInfoBean> data) {
         return (long) mListDatas.size();
+    }
+
+    @OnClick({R.id.tv_toolbar_left, R.id.tv_toolbar_right, R.id.tv_toolbar_center})
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.tv_toolbar_left:
+                // 退出当前页面
+                getActivity().finish();
+                break;
+            case R.id.tv_toolbar_right:
+                // 进入找人页面
+                Intent itFollow = new Intent(getActivity(), FindSomeOneContainerActivity.class);
+                Bundle bundleFollow = new Bundle();
+                itFollow.putExtras(bundleFollow);
+                startActivity(itFollow);
+                break;
+            case R.id.tv_toolbar_center:
+                // 进入搜索页面
+                startActivity(new Intent(getActivity(), SearchFriendsActivity.class));
+                break;
+            default:
+        }
     }
 }
