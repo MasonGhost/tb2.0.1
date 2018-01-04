c7151cf2036428378dacd601da861c3a068c8b52
CatherineFXX <648129313@qq.com>
2018-01-04 17:34:25 +0800

    feat: (#1586) 添加一个自定义搜索输入框

---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/widget/TSSearchView.java
@@ -0,0 +1,168 @@
+package com.zhiyicx.thinksnsplus.widget;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.AppCompatEditText;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+import com.zhiyicx.thinksnsplus.R;
+
+/**
+ * @author Catherine
+ * @describe
+ * @date 2018/1/4
+ * @contact email:648129313@qq.com
+ */
+
+public class TSSearchView extends AppCompatEditText implements View.OnKeyListener, View.OnFocusChangeListener, TextWatcher {
+    /**
+     * 是否显示在左边
+     */
+    private boolean isShowNormal = false;
+    /**
+     * 是否点击软键盘搜索
+     */
+    private boolean pressSearch = false;
+    /**
+     * 软键盘搜索键监听
+     */
+    private OnSearchClickListener listener;
+
+    private Drawable[] drawables; // 控件的图片资源
+    private Drawable drawableLeft, drawableDel; // 搜索图标和删除按钮图标
+
+    public void setOnSearchClickListener(OnSearchClickListener listener) {
+        this.listener = listener;
+    }
+
+    public interface OnSearchClickListener {
+        void onSearchClick(View view);
+        void onTextChanged(CharSequence s, int start, int before, int count);
+    }
+
+    public TSSearchView(Context context) {
+        this(context, null);
+        init();
+    }
+
+    public TSSearchView(Context context, AttributeSet attrs) {
+        this(context, attrs, android.R.attr.editTextStyle);
+        init();
+    }
+
+    public TSSearchView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+
+        setOnFocusChangeListener(this);
+        setOnKeyListener(this);
+        addTextChangedListener(this);
+
+        drawables = getCompoundDrawables();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        if (isShowNormal) { // 如果是默认样式，直接绘制
+            if (length() < 1) {
+                drawableDel = null;
+            }
+            this.setCompoundDrawablesWithIntrinsicBounds(drawableLeft, null, drawableDel, null);
+            super.onDraw(canvas);
+        } else { // 如果不是默认样式，需要将图标绘制在中间
+            drawableLeft = drawables[0];
+            float textWidth = getPaint().measureText(getHint().toString());
+            int drawablePadding = getCompoundDrawablePadding();
+            int drawableWidth = drawableLeft.getIntrinsicWidth();
+            float bodyWidth = textWidth + drawableWidth + drawablePadding;
+            canvas.translate((getWidth() - bodyWidth - getPaddingLeft() - getPaddingRight()) / 2, 0);
+            super.onDraw(canvas);
+        }
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        // 被点击时，恢复默认样式
+        if (!pressSearch && TextUtils.isEmpty(getText().toString())) {
+            isShowNormal = hasFocus;
+        }
+    }
+
+    @Override
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+        pressSearch = (keyCode == KeyEvent.KEYCODE_ENTER);
+        if (pressSearch && listener != null) {
+            /*隐藏软键盘*/
+            InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+            if (imm.isActive()) {
+                imm.hideSoftInputFromWindow(v.getApplicationWindowToken(), 0);
+            }
+            if (event.getAction() == KeyEvent.ACTION_UP) {
+                pressSearch = false;
+                listener.onSearchClick(v);
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 当手指抬起的位置在clean的图标的区域 我们将此视为进行清除操作
+     * getWidth():得到控件的宽度
+     * event.getX():抬起时的坐标(改坐标是相对于控件本身而言的)
+     * getTotalPaddingRight():clean的图标左边缘至控件右边缘的距离
+     * getPaddingRight():clean的图标右边缘至控件右边缘的距离
+     * 于是:
+     * getWidth() - getTotalPaddingRight()表示: 控件左边到clean的图标左边缘的区域
+     * getWidth() - getPaddingRight()表示: 控件左边到clean的图标右边缘的区域 所以这两者之间的区域刚好是clean的图标的区域
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_UP:
+                boolean isClean = (event.getX() > (getWidth() - getTotalPaddingRight()))
+                        && (event.getX() < (getWidth() - getPaddingRight()));
+                if (isClean) {
+                    setText("");
+                }
+                break;
+
+            default:
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public void afterTextChanged(Editable arg0) {
+        if (this.length() < 1) {
+            drawableDel = null;
+        } else {
+            drawableDel = this.getResources().getDrawable(R.mipmap.topic_icon_delete);
+        }
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+                                  int arg3) {
+    }
+
+    @Override
+    public void onTextChanged(CharSequence arg0, int arg1, int arg2,
+                              int arg3) {
+        if (listener != null){
+            listener.onTextChanged(arg0, arg1, arg2, arg3);
+        }
+    }
+}
