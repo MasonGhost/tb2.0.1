27cee2b1b461ddceb460316b7f515d53406e4376
CatherineFXX <648129313@qq.com>
2018-01-08 16:02:45 +0800

    feat: (#1591)  将环信的选择视频的相关页面导入项目
    目前没有选择视频的相应的设计图，先暂时将环信的加入

---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/chat/video/VideoEntity.java
@@ -0,0 +1,9 @@
+package com.zhiyicx.thinksnsplus.modules.chat.video;
+
+public class VideoEntity {
+	public int ID;
+	public String title;
+	public String filePath;
+	public int size;
+	public int duration;
+}
