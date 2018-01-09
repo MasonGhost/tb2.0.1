43f288b5f3fecf3d5764c197e956cd3f9a2c795a
狂奔的唐玄奘 <1310683823@qq.com>
2018-01-09 17:34:25 +0800

    feat: ( #1324) 苦苦挣扎着优化viewpager，未见成效

--- app/src/main/java/com/zhiyicx/thinksnsplus/modules/information/infomain/container/InfoContainerFragment.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/information/infomain/container/InfoContainerFragment.java
@@ -167,6 +167,7 @@ public class InfoContainerFragment extends TSViewPagerFragment<InfoMainContract.
                     });
             mTsvToolbar.notifyDataSetChanged(mTitle);
             tsViewPagerAdapter.bindData(mFragmentList, mTitle.toArray(new String[]{}));
+            mVpFragment.setOffscreenPageLimit(mTitle.size());
         }
 
     }
@@ -200,6 +201,7 @@ public class InfoContainerFragment extends TSViewPagerFragment<InfoMainContract.
         }
         mTsvToolbar.notifyDataSetChanged(mTitle);
         tsViewPagerAdapter.bindData(mFragmentList, mTitle.toArray(new String[]{}));
+        mVpFragment.setOffscreenPageLimit(mTitle.size());
     }
 
     @Override
