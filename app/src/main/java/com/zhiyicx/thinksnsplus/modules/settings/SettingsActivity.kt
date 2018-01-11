e886c2f5a5f8f75e8f2a8a8626ac9df86801a432
Jungle68 <335891510@qq.com>
2018-01-11 16:41:41 +0800

    feat: add kotlin support, and update SettingActivity to kotlin file

---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/settings/SettingsActivity.kt
@@ -0,0 +1,31 @@
+package com.zhiyicx.thinksnsplus.modules.settings
+
+import com.zhiyicx.baseproject.base.TSActivity
+import com.zhiyicx.thinksnsplus.base.AppApplication
+
+/**
+ * @Describe 设置
+ * @Author Jungle68
+ * @Date 2017/1/9
+ * @Contact master.jungle68@gmail.com
+ */
+
+class SettingsActivity : TSActivity<SettingsPresenter, SettingsFragment>() {
+
+    override fun componentInject() {
+        DaggerSettingsComponent
+                .builder()
+                .appComponent(AppApplication.AppComponentHolder.getAppComponent())
+                .settingsPresenterModule(SettingsPresenterModule(mContanierFragment))
+                .build()
+                .inject(this)
+
+    }
+
+    override fun getFragment(): SettingsFragment {
+        return SettingsFragment.newInstance()
+    }
+
+
+}
+
