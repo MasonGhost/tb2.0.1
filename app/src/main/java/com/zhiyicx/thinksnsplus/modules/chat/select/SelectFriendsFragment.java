276a9d4279881978f65742d1c4750022b02b62bd
CatherineFXX <648129313@qq.com>
2018-01-12 15:39:10 +0800

    feat: (#1589) 选择好友页面处理选择好友的逻辑，以及展示选中好友

--- app/src/main/java/com/zhiyicx/thinksnsplus/modules/chat/select/SelectFriendsFragment.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/chat/select/SelectFriendsFragment.java
@@ -1,11 +1,27 @@
 package com.zhiyicx.thinksnsplus.modules.chat.select;
 
+import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.jakewharton.rxbinding.view.RxView;
+import com.jakewharton.rxbinding.widget.RxTextView;
 import com.zhiyicx.baseproject.base.TSListFragment;
+import com.zhiyicx.common.utils.recycleviewdecoration.LinearDecoration;
+import com.zhiyicx.thinksnsplus.R;
 import com.zhiyicx.thinksnsplus.data.beans.UserInfoBean;
+import com.zhiyicx.thinksnsplus.modules.chat.adapter.SelectFriendsAllAdapter;
+import com.zhiyicx.thinksnsplus.modules.chat.adapter.SelectedFriendsAdapter;
+import com.zhy.adapter.recyclerview.MultiItemTypeAdapter;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import butterknife.BindView;
+import rx.functions.Action1;
 
 /**
  * @author Catherine
@@ -15,25 +31,127 @@ import java.util.List;
  */
 
 public class SelectFriendsFragment extends TSListFragment<SelectFriendsContract.Presenter, UserInfoBean>
-        implements SelectFriendsContract.View{
+        implements SelectFriendsContract.View, SelectFriendsAllAdapter.OnUserSelectedListener {
 
-    @Override
-    public void onNetResponseSuccess(List<UserInfoBean> data, boolean isLoadMore) {
+    @BindView(R.id.iv_search_icon)
+    ImageView mIvSearchIcon;
+    @BindView(R.id.rv_select_result)
+    RecyclerView mRvSelectResult;
+    @BindView(R.id.edit_search_friends)
+    AppCompatEditText mEditSearchFriends;
+    @BindView(R.id.rv_search_result)
+    RecyclerView mRvSearchResult;
 
-    }
-
-    @Override
-    public void onCacheResponseSuccess(List<UserInfoBean> data, boolean isLoadMore) {
-
-    }
-
-    @Override
-    public void refreshData(List<UserInfoBean> datas) {
-
-    }
+    private List<UserInfoBean> mSelectedList;
+    private List<UserInfoBean> mSearchResultList;
+    private SelectFriendsAllAdapter mSearchResultAdapter;
+    private SelectedFriendsAdapter mSelectedFriendsAdapter;
 
     @Override
     protected RecyclerView.Adapter getAdapter() {
-        return null;
+        SelectFriendsAllAdapter allAdapter = new SelectFriendsAllAdapter(getContext(), mListDatas, this);
+        return allAdapter;
+    }
+
+    @Override
+    protected void initView(View rootView) {
+        super.initView(rootView);
+
+        setLeftTextColor(R.color.themeColor);
+        // 选中结果
+        LinearLayoutManager selectManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        mRvSelectResult.setLayoutManager(selectManager);
+        // 搜索结果
+        LinearLayoutManager searchManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        mRvSearchResult.setLayoutManager(searchManager);
+
+        RxView.focusChanges(mEditSearchFriends)
+                .subscribe(aBoolean -> {
+                    if (aBoolean){
+
+                    }
+                });
+        RxTextView.textChanges(mEditSearchFriends)
+                .subscribe(charSequence -> {
+                    if (!TextUtils.isEmpty(charSequence)){
+                        // 搜索
+                    }
+                });
+    }
+
+    @Override
+    protected void initData() {
+        super.initData();
+        // 选中的结果
+        mSelectedList = new ArrayList<>();
+        mSelectedFriendsAdapter = new SelectedFriendsAdapter(getContext(), mSelectedList);
+        mRvSelectResult.setAdapter(mSelectedFriendsAdapter);
+        mRvSelectResult.addItemDecoration(new LinearDecoration(0, 0, 5, 5));
+        // 搜索的结果
+        mSearchResultList = new ArrayList<>();
+        mSearchResultAdapter = new SelectFriendsAllAdapter(getContext(), mSearchResultList, this);
+        mRvSearchResult.setAdapter(mSearchResultAdapter);
+        checkData();
+    }
+
+    @Override
+    protected String setRightTitle() {
+        return getString(R.string.select_friends_right_title_default);
+    }
+
+    @Override
+    protected String setCenterTitle() {
+        return getString(R.string.select_friends_center_title);
+    }
+
+    @Override
+    protected int setLeftImg() {
+        return 0;
+    }
+
+    @Override
+    protected String setLeftTitle() {
+        return getString(R.string.cancel);
+    }
+
+    @Override
+    protected void setLeftClick() {
+        getActivity().finish();
+    }
+
+    @Override
+    protected void setRightClick() {
+        // 发起聊天
+    }
+
+    @Override
+    protected int getBodyLayoutId() {
+        return R.layout.fragment_select_friends;
+    }
+
+    /**
+     * 聊天按钮是否可以点击
+     */
+    private void checkData(){
+        mToolbarRight.setEnabled(mSelectedList.size() != 0);
+        if (mSelectedList.size() > 0){
+            setRightText(String.format(getString(R.string.select_friends_right_title), mSelectedList.size()));
+            mToolbarRight.setTextColor(getColor(R.color.themeColor));
+        } else {
+            setRightText(getString(R.string.select_friends_right_title_default));
+            mToolbarRight.setTextColor(getColor(R.color.normal_for_disable_button_text));
+        }
+        mSelectedFriendsAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onUserSelected(UserInfoBean userInfoBean, int position) {
+        // 选中
+        if (userInfoBean.isSelected()){
+            mSelectedList.add(userInfoBean);
+        } else {
+            mSelectedList.remove(userInfoBean);
+        }
+        checkData();
     }
 }
--- app/src/main/java/com/zhiyicx/thinksnsplus/modules/chat/select/SelectFriendsPresenter.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/chat/select/SelectFriendsPresenter.java
@@ -1,13 +1,18 @@
 package com.zhiyicx.thinksnsplus.modules.chat.select;
 
+import com.zhiyicx.common.utils.log.LogUtils;
 import com.zhiyicx.thinksnsplus.base.AppBasePresenter;
+import com.zhiyicx.thinksnsplus.base.BaseSubscribeForV2;
 import com.zhiyicx.thinksnsplus.data.beans.UserInfoBean;
+import com.zhiyicx.thinksnsplus.data.source.local.UserInfoBeanGreenDaoImpl;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
 import javax.inject.Inject;
+
+import rx.Subscription;
 
 /**
  * @author Catherine
@@ -20,18 +25,41 @@ public class SelectFriendsPresenter extends AppBasePresenter<SelectFriendsContra
         implements SelectFriendsContract.Presenter{
 
     @Inject
+    protected UserInfoBeanGreenDaoImpl mUserInfoBeanGreenDao;
+
+    @Inject
     public SelectFriendsPresenter(SelectFriendsContract.Repository repository, SelectFriendsContract.View rootView) {
         super(repository, rootView);
     }
 
     @Override
     public void requestNetData(Long maxId, boolean isLoadMore) {
+        Subscription subscription = mRepository.getUserFriendsList(maxId, "")
+                .subscribe(new BaseSubscribeForV2<List<UserInfoBean>>() {
+                    @Override
+                    protected void onSuccess(List<UserInfoBean> data) {
+                        mRootView.onNetResponseSuccess(data, isLoadMore);
+                    }
 
+                    @Override
+                    protected void onFailure(String message, int code) {
+                        Throwable throwable = new Throwable(message);
+                        mRootView.onResponseError(throwable, isLoadMore);
+                    }
+
+                    @Override
+                    protected void onException(Throwable throwable) {
+                        LogUtils.e(throwable, throwable.getMessage());
+                        mRootView.onResponseError(throwable, isLoadMore);
+                    }
+                });
+        addSubscrebe(subscription);
     }
 
     @Override
     public void requestCacheData(Long maxId, boolean isLoadMore) {
-
+        List<UserInfoBean> followFansBeanList = mUserInfoBeanGreenDao.getUserFriendsList(maxId);
+        mRootView.onCacheResponseSuccess(followFansBeanList, isLoadMore);
     }
 
     @Override
