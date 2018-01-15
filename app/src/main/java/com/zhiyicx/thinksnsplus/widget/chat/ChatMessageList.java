8f4b0bd9d377bf18d6fed3f18dce580f4b4f58d5
狂奔的唐玄奘 <1310683823@qq.com>
2018-01-15 16:27:55 +0800

    feat: ( #1324) 部分手机聊天列表弹起软键盘会有问题

--- app/src/main/java/com/zhiyicx/thinksnsplus/widget/chat/ChatMessageList.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/widget/chat/ChatMessageList.java
@@ -121,7 +121,7 @@ public class ChatMessageList extends FrameLayout implements OnRefreshListener {
         mRecyclerView = (RecyclerView) findViewById(R.id.swipe_target);
         mLinearLayoutManager = new LinearLayoutManager(context);
         // 让最后一个item始终在recycleview内，解决当item过长，弹起键盘recycleview被遮挡的问题
-        mLinearLayoutManager.setStackFromEnd(true);
+//        mLinearLayoutManager.setStackFromEnd(true);
         mRecyclerView.setLayoutManager(mLinearLayoutManager);
 //        mRecyclerView.addItemDecoration(new LinearDecoration(0, ConvertUtils.dp2px(getContext(), RECYCLEVIEW_ITEMDECORATION_SPACING), 0, 0),-1);//设置最后一个 Item 的间隔
         //如果可以确定每个item的高度是固定的，设置这个选项可以提高性能
