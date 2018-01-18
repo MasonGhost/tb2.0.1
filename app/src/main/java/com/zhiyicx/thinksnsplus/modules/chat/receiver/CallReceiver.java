bf741d85425fb4539993936723a50effa168c4b9
CatherineFXX <648129313@qq.com>
2018-01-18 17:59:14 +0800

    feat: ( #1591 ) 添加视频通话页面
    还有很多细节仍需完善QAQ

--- app/src/main/java/com/zhiyicx/thinksnsplus/modules/chat/receiver/CallReceiver.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/chat/receiver/CallReceiver.java
@@ -20,6 +20,7 @@ import android.content.Intent;
 
 import com.hyphenate.chat.EMClient;
 import com.hyphenate.util.EMLog;
+import com.zhiyicx.thinksnsplus.modules.chat.call.VideoCallActivity;
 import com.zhiyicx.thinksnsplus.modules.chat.call.VoiceCallActivity;
 
 public class CallReceiver extends BroadcastReceiver {
@@ -34,9 +35,9 @@ public class CallReceiver extends BroadcastReceiver {
         //call type
         String type = intent.getStringExtra("type");
         if ("video".equals(type)) { //video call
-//		    context.startActivity(new Intent(context, VideoCallActivity.class).
-//                    putExtra("username", from).putExtra("isComingCall", true).
-//                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+		    context.startActivity(new Intent(context, VideoCallActivity.class).
+                    putExtra("username", from).putExtra("isComingCall", true).
+                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
         } else { //voice call
             context.startActivity(new Intent(context, VoiceCallActivity.class).
                     putExtra("username", from).putExtra("isComingCall", true).
