3ff0287e0c967429224539040994efc45b4388c9
CatherineFXX <648129313@qq.com>
2018-01-24 16:55:44 +0800

    feat: ( #1591 ) 成员列表添加群主标识

--- app/src/main/java/com/zhiyicx/thinksnsplus/modules/chat/adapter/ChatMemberAdapter.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/chat/adapter/ChatMemberAdapter.java
@@ -1,6 +1,7 @@
 package com.zhiyicx.thinksnsplus.modules.chat.adapter;
 
 import android.content.Context;
+import android.view.View;
 
 import com.hyphenate.easeui.bean.ChatUserInfoBean;
 import com.zhiyicx.baseproject.widget.UserAvatarView;
@@ -11,6 +12,7 @@ import com.zhy.adapter.recyclerview.CommonAdapter;
 import com.zhy.adapter.recyclerview.base.ViewHolder;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author Catherine
@@ -21,9 +23,11 @@ import java.util.List;
 
 public class ChatMemberAdapter extends CommonAdapter<UserInfoBean>{
 
+    private Long mOwnerId;
 
-    public ChatMemberAdapter(Context context, List<UserInfoBean> datas) {
+    public ChatMemberAdapter(Context context, List<UserInfoBean> datas, long ownerId) {
         super(context, R.layout.item_chat_member, datas);
+        this.mOwnerId = ownerId;
     }
 
     @Override
@@ -32,12 +36,20 @@ public class ChatMemberAdapter extends CommonAdapter<UserInfoBean>{
         if (chatUserInfoBean.getUser_id() == -1L){
             // 加号
             ivUserPortrait.getIvAvatar().setImageResource(R.mipmap.btn_chatdetail_add);
+            holder.setVisible(R.id.tv_owner_flag, View.GONE);
+            holder.setVisible(R.id.tv_user_name, View.GONE);
+            ivUserPortrait.getIvVerify().setVisibility(View.GONE);
         } else if (chatUserInfoBean.getUser_id() == -2L){
             ivUserPortrait.getIvAvatar().setImageResource(R.mipmap.btn_chatdetail_reduce);
+            holder.setVisible(R.id.tv_owner_flag, View.GONE);
+            holder.setVisible(R.id.tv_user_name, View.GONE);
+            ivUserPortrait.getIvVerify().setVisibility(View.GONE);
         } else {
             ImageUtils.loadUserHead(chatUserInfoBean, ivUserPortrait, false);
             holder.setText(R.id.tv_user_name, chatUserInfoBean.getName());
+            holder.setVisible(R.id.tv_owner_flag, mOwnerId.equals(chatUserInfoBean.getUser_id()) ? View.VISIBLE : View.GONE);
+            holder.setVisible(R.id.tv_user_name, View.VISIBLE);
+            ivUserPortrait.getIvVerify().setVisibility(View.VISIBLE);
         }
-
     }
 }
