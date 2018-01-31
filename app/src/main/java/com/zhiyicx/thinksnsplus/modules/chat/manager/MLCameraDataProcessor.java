44619fa9cdef087f8b675bcd29364485677acb53
狂奔的唐玄奘 <1310683823@qq.com>
2018-01-31 18:13:45 +0800

    feat: ( #1457) 聊天管理类

---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/chat/manager/MLCameraDataProcessor.java
@@ -0,0 +1,35 @@
+package com.zhiyicx.thinksnsplus.modules.chat.manager;
+
+import android.hardware.Camera;
+
+import com.hyphenate.chat.EMCallManager;
+
+/**
+ * Created by lzan13 on 2016/8/9.
+ */
+public class MLCameraDataProcessor implements EMCallManager.EMCameraDataProcessor {
+
+    byte yDelta = 0;
+
+    synchronized void setYDelta(byte yDelta) {
+        this.yDelta = yDelta;
+    }
+
+    /**
+     * @author Jliuer
+     * @Date 18/01/31 17:54
+     * @Email Jliuer@aliyun.com
+     * @Description @Link RtcConnection 2015
+     */
+    @Override
+    public synchronized void onProcessData(byte[] bytes, Camera camera, int width, int height, int rotation) {
+        int wh = width * height;
+        for (int i = 0; i < wh; i++) {
+            int d = (bytes[i] & 0xFF) + yDelta;
+            d = d < 16 ? 16 : d;
+            d = d > 235 ? 235 : d;
+            bytes[i] = (byte) d;
+        }
+    }
+
+}
