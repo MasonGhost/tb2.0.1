042406b3fe7433237d547a8a43a57bce0ced68b2
狂奔的唐玄奘 <1310683823@qq.com>
2018-02-07 16:58:41 +0800

    feat: ( #1457) 通话权限请求

--- app/src/main/java/com/zhiyicx/thinksnsplus/modules/chat/callV2/receiver/TSEMCallReceiver.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/chat/callV2/receiver/TSEMCallReceiver.java
@@ -1,10 +1,12 @@
 package com.zhiyicx.thinksnsplus.modules.chat.callV2.receiver;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
 import com.hyphenate.chat.EMClient;
+import com.tbruyelle.rxpermissions.RxPermissions;
 import com.zhiyicx.baseproject.em.manager.TSEMCallStatus;
 import com.zhiyicx.baseproject.em.manager.control.TSEMConstants;
 import com.zhiyicx.thinksnsplus.modules.chat.callV2.BaseCallActivity;
@@ -29,6 +31,8 @@ public class TSEMCallReceiver extends BroadcastReceiver {
      */
     private String TYPE_VOICE = "voice";
 
+    private RxPermissions mRxPermissions;
+
     public TSEMCallReceiver() {
     }
 
@@ -49,17 +53,44 @@ public class TSEMCallReceiver extends BroadcastReceiver {
         // 呼叫接收方
         String callTo = intent.getStringExtra(TSEMConstants.TS_EXTRA_TO);
 
+        mRxPermissions = new RxPermissions(TSEMHyphenate.getInstance().getTopActivity());
+
         // 判断下当前被呼叫的为自己的时候才启动通话界面 TODO 这个当不同appkey下相同的username时就无效了
         if (callTo.equals(EMClient.getInstance().getCurrentUser())) {
             // 根据通话类型跳转到语音通话或视频通话界面
             if (callType.equals(TYPE_VIDEO)) {
-                BaseCallActivity.startVideoCallActivity(context, callFrom, true);
-                // 设置当前通话类型为视频通话
-                TSEMCallStatus.getInstance().setCallType(TSEMCallStatus.CALL_TYPE_VIDEO);
+
+                mRxPermissions
+                        .requestEach(Manifest.permission.CAMERA)
+                        .subscribe(permission -> {
+                            if (permission.granted) {
+                                // 权限被允许
+                                BaseCallActivity.startVideoCallActivity(context, callFrom, true);
+                                // 设置当前通话类型为视频通话
+                                TSEMCallStatus.getInstance().setCallType(TSEMCallStatus.CALL_TYPE_VIDEO);
+                            } else if (permission.shouldShowRequestPermissionRationale) {
+                                // 权限没有被彻底禁止
+                            } else {
+                            }
+                        });
+
+
             } else if (callType.equals(TYPE_VOICE)) {
-                BaseCallActivity.startVoiceCallActivity(context, callFrom, true);
-                // 设置当前通话类型为语音通话
-                TSEMCallStatus.getInstance().setCallType(TSEMCallStatus.CALL_TYPE_VOICE);
+
+                mRxPermissions
+                        .requestEach(Manifest.permission.RECORD_AUDIO)
+                        .subscribe(permission -> {
+                            if (permission.granted) {
+                                // 权限被允许
+                                BaseCallActivity.startVoiceCallActivity(context, callFrom, true);
+                                // 设置当前通话类型为语音通话
+                                TSEMCallStatus.getInstance().setCallType(TSEMCallStatus.CALL_TYPE_VOICE);
+                            } else if (permission.shouldShowRequestPermissionRationale) {
+                                // 权限没有被彻底禁止
+                            } else {
+                                // 权限被彻底禁止
+                            }
+                        });
             }
         }
     }
