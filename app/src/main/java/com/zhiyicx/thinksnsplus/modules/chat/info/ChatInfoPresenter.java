061261fb1ceeb9f4c275bf1776976070689bc382
Jungle68 <335891510@qq.com>
2018-02-07 11:13:48 +0800

    chore: 退群，更新错误提示

--- app/src/main/java/com/zhiyicx/thinksnsplus/modules/chat/info/ChatInfoPresenter.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/chat/info/ChatInfoPresenter.java
@@ -10,6 +10,7 @@ import com.zhiyicx.thinksnsplus.base.AppApplication;
 import com.zhiyicx.thinksnsplus.base.AppBasePresenter;
 import com.zhiyicx.thinksnsplus.base.BaseSubscribeForV2;
 import com.zhiyicx.thinksnsplus.base.EmptySubscribe;
+import com.zhiyicx.thinksnsplus.config.DefaultUserInfoConfig;
 import com.zhiyicx.thinksnsplus.config.EventBusTagConfig;
 import com.zhiyicx.thinksnsplus.data.beans.ChatGroupBean;
 import com.zhiyicx.thinksnsplus.data.beans.UserInfoBean;
@@ -80,8 +81,7 @@ public class ChatInfoPresenter extends AppBasePresenter<ChatInfoContract.View>
                         }
                         return Observable.just(id);
                     } catch (HyphenateException e) {
-                        e.printStackTrace();
-                        return Observable.error(null);
+                        return Observable.error(e);
                     }
 
                 })
@@ -283,7 +283,7 @@ public class ChatInfoPresenter extends AppBasePresenter<ChatInfoContract.View>
         try {
             return mUserInfoBeanGreenDao.getSingleDataFromCache(Long.parseLong(id));
         } catch (NumberFormatException e) {
-            return new UserInfoBean("未知用户");
+            return DefaultUserInfoConfig.getDefaultDeletUserInfo(mContext,0);
         }
     }
 }
