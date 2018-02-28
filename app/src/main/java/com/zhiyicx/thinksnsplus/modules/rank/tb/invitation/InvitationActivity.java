02992deb7f846f0aafc75ebd429e2b314cc64277
狂奔的唐玄奘 <1310683823@qq.com>
2018-02-28 15:25:33 +0800

    secondary develop: 邀请界面搭建

---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/rank/tb/invitation/InvitationActivity.java
@@ -0,0 +1,21 @@
+package com.zhiyicx.thinksnsplus.modules.rank.tb.invitation;
+
+import com.zhiyicx.baseproject.base.TSActivity;
+
+/**
+ * @Author Jliuer
+ * @Date 2018/02/28/14:44
+ * @Email Jliuer@aliyun.com
+ * @Description
+ */
+public class InvitationActivity extends TSActivity<InvitationPresenter,InvitationFragment> {
+    @Override
+    protected InvitationFragment getFragment() {
+        return null;
+    }
+
+    @Override
+    protected void componentInject() {
+
+    }
+}
---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/rank/tb/invitation/InvitationFragment.java
@@ -0,0 +1,30 @@
+package com.zhiyicx.thinksnsplus.modules.rank.tb.invitation;
+
+import android.view.View;
+
+import com.zhiyicx.baseproject.base.TSFragment;
+import com.zhiyicx.thinksnsplus.R;
+
+/**
+ * @Author Jliuer
+ * @Date 2018/02/28/14:44
+ * @Email Jliuer@aliyun.com
+ * @Description
+ */
+public class InvitationFragment extends TSFragment<InvitationContract.Presenter> implements InvitationContract.View{
+
+    @Override
+    protected void initView(View rootView) {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected int getBodyLayoutId() {
+        return R.layout.fragment_invitation;
+    }
+}
