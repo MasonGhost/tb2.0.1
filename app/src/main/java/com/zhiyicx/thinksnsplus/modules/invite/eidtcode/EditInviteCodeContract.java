1f9393316454538ad226083ec8fff4ee41164c80
Jungle68 <335891510@qq.com>
2018-02-28 14:52:27 +0800

    secondary develop : 增加填写邀请码页面基本框架

---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/invite/eidtcode/EditInviteCodeContract.java
@@ -0,0 +1,28 @@
+package com.zhiyicx.thinksnsplus.modules.invite.eidtcode;
+
+import com.zhiyicx.common.mvp.i.IBasePresenter;
+import com.zhiyicx.common.mvp.i.IBaseView;
+
+/**
+ * @Describe
+ * @Author Jungle68
+ * @Date 2017/2/28
+ * @Contact master.jungle68@gmail.com
+ */
+
+public interface EditInviteCodeContract {
+
+    interface View extends IBaseView<Presenter> {
+
+    }
+
+    interface Presenter extends IBasePresenter {
+        /**
+         * 提交邀请码
+         *
+         * @param inviteCode 邀请码
+         */
+        void submitInviteCode(String inviteCode);
+    }
+
+}
