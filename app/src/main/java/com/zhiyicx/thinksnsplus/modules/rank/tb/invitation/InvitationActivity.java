d7a5ea68bb491e9e88593b7722a659a86a819584
狂奔的唐玄奘 <1310683823@qq.com>
2018-03-01 14:45:46 +0800

    secondary develop: 邀请页界面部分逻辑

--- app/src/main/java/com/zhiyicx/thinksnsplus/modules/rank/tb/invitation/InvitationActivity.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/rank/tb/invitation/InvitationActivity.java
@@ -1,6 +1,7 @@
 package com.zhiyicx.thinksnsplus.modules.rank.tb.invitation;
 
 import com.zhiyicx.baseproject.base.TSActivity;
+import com.zhiyicx.thinksnsplus.base.AppApplication;
 
 /**
  * @Author Jliuer
@@ -8,14 +9,18 @@ import com.zhiyicx.baseproject.base.TSActivity;
  * @Email Jliuer@aliyun.com
  * @Description
  */
-public class InvitationActivity extends TSActivity<InvitationPresenter,InvitationFragment> {
+public class InvitationActivity extends TSActivity<InvitationPresenter, InvitationFragment> {
+
     @Override
     protected InvitationFragment getFragment() {
-        return null;
+        return InvitationFragment.newInstance(getIntent().getExtras());
     }
 
     @Override
     protected void componentInject() {
-
+        DaggerInvitationComponent.builder()
+                .appComponent(AppApplication.AppComponentHolder.getAppComponent())
+                .invitationPresenterModule(new InvitationPresenterModule(mContanierFragment))
+                .build().inject(this);
     }
 }
