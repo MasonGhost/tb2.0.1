6d8533792c1a273c87aadcdfa58094eb29d80952
狂奔的唐玄奘 <1310683823@qq.com>
2018-03-09 16:07:20 +0800

    feat: ( #1324) 消息时间异常处理

--- app/src/main/java/com/zhiyicx/thinksnsplus/modules/home/message/messagelist/EmTimeSortClass.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/home/message/messagelist/EmTimeSortClass.java
@@ -7,17 +7,23 @@ import com.zhiyicx.thinksnsplus.utils.BaseTimeStringSortClass;
  * @Author Jliuer
  * @Date 2018/03/09/9:47
  * @Email Jliuer@aliyun.com
- * @Description
+ * @Description 聊天消息按时间排序
  */
 public class EmTimeSortClass extends BaseTimeStringSortClass<MessageItemBeanV2> {
 
     @Override
     protected String getData1Time(MessageItemBeanV2 data) {
+        if (data.getConversation().getLastMessage()==null){
+            return System.currentTimeMillis()+"";
+        }
         return data.getConversation().getLastMessage().getMsgTime() + "";
     }
 
     @Override
     protected String getData2Time(MessageItemBeanV2 data) {
+        if (data.getConversation().getLastMessage()==null){
+            return System.currentTimeMillis()+"";
+        }
         return data.getConversation().getLastMessage().getMsgTime() + "";
     }
 }
