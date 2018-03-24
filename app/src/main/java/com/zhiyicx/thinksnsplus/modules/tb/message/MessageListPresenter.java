01afd15ec488baa6bfaa989bb9e0600b0df7747f
Jungle68 <335891510@qq.com>
2018-03-24 20:15:16 +0800

    secondary develop: 修改机构主页置顶布局

--- app/src/main/java/com/zhiyicx/thinksnsplus/modules/tb/message/MessageListPresenter.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/tb/message/MessageListPresenter.java
@@ -1,6 +1,8 @@
 package com.zhiyicx.thinksnsplus.modules.tb.message;
 
+import com.google.gson.Gson;
 import com.zhiyicx.thinksnsplus.base.AppBasePresenter;
+import com.zhiyicx.thinksnsplus.base.BaseSubscribeForV2;
 import com.zhiyicx.thinksnsplus.config.EventBusTagConfig;
 import com.zhiyicx.thinksnsplus.config.JpushMessageTypeConfig;
 import com.zhiyicx.thinksnsplus.data.beans.HintSideBarUserBean;
@@ -16,11 +18,16 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
+
 /**
  * Created by lx on 2018/3/24.
  */
 
-public class MessageListPresenter extends AppBasePresenter<MessageListContract.View> implements MessageListContract.Presenter{
+public class MessageListPresenter extends AppBasePresenter<MessageListContract.View> implements MessageListContract.Presenter {
 
     @Inject
     UserInfoRepository mUserInfoRepository;
@@ -54,6 +61,30 @@ public class MessageListPresenter extends AppBasePresenter<MessageListContract.V
         if (jpushMessageBean.getType() == null) {
             return;
         }
+        Observable.just(jpushMessageBean)
+                .subscribeOn(Schedulers.io())
+                .map(new Func1<JpushMessageBean, TbMessageBean>() {
+                    @Override
+                    public TbMessageBean call(JpushMessageBean jpushMessageBean) {
+                        Gson gson = new Gson();
+                        TbMessageBean tbMessageBean = null;
+                        try {
+                            tbMessageBean = gson.fromJson(gson.toJson(jpushMessageBean.getExtras()), TbMessageBean.class);
+                            //
+                        } catch (Exception ignored) {
+
+                        }
+                        return tbMessageBean;
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new BaseSubscribeForV2<TbMessageBean>() {
+                    @Override
+                    protected void onSuccess(TbMessageBean data) {
+                
+                    }
+                });
+
 
     }
 }
